91b480e68aa42d6abdb88730151754832dc0677d
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-04-23 17:39:45 +0300

    Add files via upload

---
+++ 01.Programming-Basics-C#/04.Conditional-Statements-Exercise/03.TimePlus15Minutes/Program.cs
@@ -0,0 +1,31 @@
+﻿//Input
+int hours = int.Parse(Console.ReadLine());
+int minutes = int.Parse(Console.ReadLine());
+
+//Calculations
+//Minutes
+if (minutes >= 45) //14:47 -> 14:62 -> 15:02
+{
+    hours++; //++ -> Добавяме 1 към променливата
+    minutes = (minutes + 15) % 60;
+}
+else
+{
+    minutes += 15; //Вместо minutes = minutes + 15; изписваме minutes += 15; -> Така се добавя стойност по-кратко към дадена промнелива
+}
+
+//Hours
+if (hours == 24) //23:47 -> 0:02
+{
+    hours = 0;
+}
+
+//Output
+if (minutes < 10)
+{
+    Console.WriteLine($"{hours}:0{minutes}");
+}
+else
+{
+    Console.WriteLine($"{hours}:{minutes}");
+}
