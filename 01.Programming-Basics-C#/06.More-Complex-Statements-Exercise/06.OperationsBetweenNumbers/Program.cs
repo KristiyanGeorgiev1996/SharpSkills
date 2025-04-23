8c3273811e182af79e813a992536b1d7aa0c1e29
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-04-23 17:57:24 +0300

    Add files via upload

---
+++ 01.Programming-Basics-C#/06.More-Complex-Statements-Exercise/06.OperationsBetweenNumbers/Program.cs
@@ -0,0 +1,75 @@
+﻿//Input
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+char operation = char.Parse(Console.ReadLine());
+
+//Calculation
+double result = 0;
+bool isEven = false;
+
+if (operation == '+')
+{
+    result = a + b;
+    isEven = result % 2 == 0;
+
+    if (isEven)
+    {
+        Console.WriteLine($"{a} + {b} = {result} - even");
+    }
+    else
+    {
+        Console.WriteLine($"{a} + {b} = {result} - odd");
+    }
+}
+else if (operation == '-')
+{
+    result = a - b;
+    isEven = result % 2 == 0;
+
+    if (isEven)
+    {
+        Console.WriteLine($"{a} - {b} = {result} - even");
+    }
+    else
+    {
+        Console.WriteLine($"{a} - {b} = {result} - odd");
+    }
+}
+else if (operation == '*')
+{
+    result = a * b;
+    isEven = result % 2 == 0;
+
+    if (isEven)
+    {
+        Console.WriteLine($"{a} * {b} = {result} - even");
+    }
+    else
+    {
+        Console.WriteLine($"{a} * {b} = {result} - odd");
+    }
+}
+else if (operation == '/')
+{
+    if (b == 0)
+    {
+        Console.WriteLine($"Cannot divide {a} by zero");
+    }
+    else
+    {
+        result = a / (double)b;
+        Console.WriteLine($"{a} / {b} = {result:f2}");
+    }
+}
+else if (operation == '%')
+{
+    if (b == 0)
+    {
+        Console.WriteLine($"Cannot divide {a} by zero");
+    }
+    else
+    {
+        result = a % b;
+        Console.WriteLine($"{a} % {b} = {result}");
+    }
+}
