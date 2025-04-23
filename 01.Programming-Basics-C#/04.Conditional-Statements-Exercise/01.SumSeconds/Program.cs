82fdd7c232bbcb2d1b3573e8952bbf3816384127
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-04-23 17:39:07 +0300

    Add files via upload

---
+++ 01.Programming-Basics-C#/04.Conditional-Statements-Exercise/01.SumSeconds/Program.cs
@@ -0,0 +1,19 @@
+﻿//Input
+int firstPlayerTime = int.Parse(Console.ReadLine());
+int secondPlayerTime = int.Parse(Console.ReadLine());
+int thirdPlayerTime = int.Parse(Console.ReadLine());
+
+//Time Cslculation
+int timeInSeconds = firstPlayerTime + secondPlayerTime + thirdPlayerTime;
+int minutes = timeInSeconds / 60; //124 / 60 = 2 minutes
+int seconds = timeInSeconds % 60; //124 % 60 = 4 seconds
+
+//Output
+if (seconds < 10)
+{
+    Console.WriteLine($"{minutes}:0{seconds}");
+}
+else
+{
+    Console.WriteLine($"{minutes}:{seconds}");
+}
