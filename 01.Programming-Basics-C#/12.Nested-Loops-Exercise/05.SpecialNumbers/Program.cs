e81abd202d09514544c18b7dc9ba17f7e6e16ac4
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-04-23 18:39:06 +0300

    Add files via upload

---
+++ 01.Programming-Basics-C#/12.Nested-Loops-Exercise/05.SpecialNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿//Input
+int n = int.Parse(Console.ReadLine());
+
+//Calculations
+for (int currentNum = 1111; currentNum <= 9999; currentNum++)
+{
+    bool isSpecial = true;
+    string currentNumAsString = currentNum.ToString();
+
+    for (int j = 0; j < currentNumAsString.Length; j++)
+    {
+        int currentDigit = int.Parse(currentNumAsString[j].ToString());
+
+        if (currentDigit == 0 || n % currentDigit != 0)
+        {
+            isSpecial = false;
+            break;
+        }
+    }
+
+    //Output
+    if (isSpecial)
+    {
+        Console.Write(currentNum + " ");
+    }
+}
