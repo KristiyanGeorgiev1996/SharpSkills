31ac88221c7cc0f354c2e73f496963a784fb9d11
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-04-23 18:37:53 +0300

    Add files via upload

---
+++ 01.Programming-Basics-C#/12.Nested-Loops-Exercise/01.NumberPyramid/Program.cs
@@ -0,0 +1,32 @@
+﻿//Input
+int n = int.Parse(Console.ReadLine());
+
+//Pyramid Print
+int currentNumber = 0;
+bool isBigger = false;
+
+//Rows
+for (int row = 1; row <= n; row++)
+{
+    //Cols
+    for (int col = 1; col <= row; col++)
+    {
+        currentNumber++;
+
+        if (currentNumber > n)
+        {
+            isBigger = true;
+            break;
+        }
+
+        //Output
+        Console.Write(currentNumber + " ");
+    }
+
+    if (isBigger)
+    {
+        break;
+    }
+
+    Console.WriteLine(); //Next row
+}
