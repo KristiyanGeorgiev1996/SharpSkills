e673cd1f8ceb6c95bde56f41cb64c802bd9a0c94
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-04-23 17:55:53 +0300

    Add files via upload

---
+++ 01.Programming-Basics-C#/06.More-Complex-Statements-Exercise/01.Cinema/Program.cs
@@ -0,0 +1,28 @@
+﻿//Input
+string projectionType = Console.ReadLine();
+int rowsCount = int.Parse(Console.ReadLine());
+int colsCount = int.Parse(Console.ReadLine());
+
+//Prices
+double premierePrice = 12;
+double normalPrice = 7.5;
+double discountPrice = 5;
+
+//Calculations
+double income = 0;
+
+switch (projectionType)
+{
+    case "Premiere":
+        income = rowsCount * colsCount * premierePrice;
+        break;
+    case "Normal":
+        income = rowsCount * colsCount * normalPrice;
+        break;
+    case "Discount":
+        income = rowsCount * colsCount * discountPrice;
+        break;
+}
+
+//Output
+Console.WriteLine($"{income:f2} leva");
