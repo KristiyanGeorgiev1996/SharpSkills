a93a2031f1be33dad7304c98151db465fe61aae2
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-04-24 17:39:02 +0300

    Add files via upload

---
+++ 02.Programming-for-QA-C#/02.Programming-Advanced-for-QA/03.Dictionaries-Lambda-and-LINQ/02.OddOccurrences/Program.cs
@@ -0,0 +1,29 @@
+﻿
+List<string> words = Console.ReadLine()
+                            .Split(' ')
+                            .Select(w => w.ToLower()) // прави всички букви малки във всяка дума
+                            .ToList();
+
+Dictionary<string, int> numbersAndCount = new Dictionary<string, int>();
+
+for (int i = 0; i < words.Count; i++)
+{
+    string key = words[i];
+
+    if (numbersAndCount.ContainsKey(key)) // ако съдържа този ключ, увеличи value с 1
+    {
+        numbersAndCount[key]++;
+    }
+    else // ако не съдържа ключа, добави го
+    {
+        numbersAndCount.Add(key, 1);
+    }
+}
+
+foreach (var item in numbersAndCount)
+{
+    if (item.Value % 2 != 0) // печатаме само ключове с нечетна стойност срещу него
+    {
+        Console.Write(item.Key + " ");
+    }
+}
