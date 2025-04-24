00a245f3ce9ad6aad8b6f8f7da2ba5130dab41f6
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-04-24 17:29:14 +0300

    Add files via upload

---
+++ 02.Programming-for-QA-C#/02.Programming-Advanced-for-QA/01.Text-Processing/05.Digits,LettersАndOther/Program.cs
@@ -0,0 +1,26 @@
+﻿
+string input = Console.ReadLine();
+
+string digits = "";
+string letters = "";
+string characters = "";
+
+for (int i = 0; i < input.Length; i++)
+{
+    if (char.IsDigit(input[i]))
+    {
+        digits += input[i];
+    }
+    else if (char.IsLetter(input[i]))
+    {
+        letters += input[i];
+    }
+    else
+    {
+        characters += input[i];
+    }
+}
+
+Console.WriteLine(digits);
+Console.WriteLine(letters);
+Console.WriteLine(characters);
