ee7032415fde6172820ec87ada9b0a4e11432dcb
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-04-24 17:45:21 +0300

    Add files via upload

---
+++ 02.Programming-for-QA-C#/02.Programming-Advanced-for-QA/04.Exercise-Dictionaries-Lambda-and-LINQ/02.AMinerTask/Program.cs
@@ -0,0 +1,25 @@
+﻿
+Dictionary<string, int> mineResources = new();
+
+string resourceName = Console.ReadLine();
+
+while (resourceName != "stop")
+{
+    int quantity = int.Parse(Console.ReadLine());
+
+    // ако ключа не съществува, го добавяме с value нула
+    if (!mineResources.ContainsKey(resourceName))
+    {
+        mineResources.Add(resourceName, 0);
+    }
+
+    // всеки път добавяме количеството към съществуващ ключ
+    mineResources[resourceName] += quantity;
+
+    resourceName = Console.ReadLine();
+}
+
+foreach (var kvp in mineResources)
+{
+    Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+}
