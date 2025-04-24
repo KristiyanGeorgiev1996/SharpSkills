e5713243b5e80e174347c7059cafcf56fbe89472
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-04-24 17:39:50 +0300

    Add files via upload

---
+++ 02.Programming-for-QA-C#/02.Programming-Advanced-for-QA/03.Dictionaries-Lambda-and-LINQ/04.WordFilter/Program.cs
@@ -0,0 +1,18 @@
+﻿string[] words = Console.ReadLine()
+                       .Split(" ")
+                       .Where(w => w.Length % 2 == 0)
+                       .ToArray();
+
+foreach (var word in words)
+{
+    Console.WriteLine(word);
+}
+
+
+// цялото решение на един ред,
+
+//Console.ReadLine()
+//.split(" ")
+//.where(w => w.length % 2 == 0)
+//.tolist()
+//.foreach (console.writeline) ;
