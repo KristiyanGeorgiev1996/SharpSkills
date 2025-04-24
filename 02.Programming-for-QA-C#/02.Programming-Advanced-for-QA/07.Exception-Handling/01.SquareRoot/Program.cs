be54189b19affb635c11298753ba7deec6213652
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-04-24 18:07:55 +0300

    Add files via upload

---
+++ 02.Programming-for-QA-C#/02.Programming-Advanced-for-QA/07.Exception-Handling/01.SquareRoot/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _01.SquareRoot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (number < 0)
+                {
+                    throw new ArgumentNullException("Invalid number.");
+                }
+
+                double sqrt = Math.Sqrt(number);
+                Console.WriteLine(sqrt);
+            }
+            catch (Exception ex) // тук влизаме само ако е хвърлена грешка
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally // този код се изпълнява независимо дали е възникнала грешка или не
+            {
+                Console.WriteLine("Goodbye.");
+            }
+
+        }
+    }
+}
