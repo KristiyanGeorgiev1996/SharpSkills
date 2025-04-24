0dd1e73df1b97a0daf549a54d1be7787078054f0
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-04-24 17:28:24 +0300

    Add files via upload

---
+++ 02.Programming-for-QA-C#/02.Programming-Advanced-for-QA/01.Text-Processing/03.Substring/Program.cs
@@ -0,0 +1,16 @@
+﻿
+string textToRemove = Console.ReadLine();
+string text = Console.ReadLine();
+
+// проверяваме дали текста съдържа текста за премахване
+while (text.Contains(textToRemove))
+{
+    // вземаме стартовия индекса на текста за премахване
+    int startIndex = text.IndexOf(textToRemove);
+
+    // премахваме от този индекс, 
+    // толкова елементи след него, колкото е дължината
+    text = text.Remove(startIndex, textToRemove.Length);
+}
+
+Console.WriteLine(text);
