882ef58bddb28b33b266d024407cd48505e92678
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-04-24 17:33:00 +0300

    Add files via upload

---
+++ 02.Programming-for-QA-C#/02.Programming-Advanced-for-QA/02.Regular-Expressions/01.MatchFullName/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Text.RegularExpressions;
+
+string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+";
+
+string input = Console.ReadLine();
+
+MatchCollection regexMatches = Regex.Matches(input, pattern);
+
+// по-подробен вариант за принтиране на намерените съвпадения
+foreach (Match item in regexMatches)
+{
+    Console.Write(item + " ");
+}
+
+// по-кратък вариант за принтиране на намерените съвпадения
+//Console.WriteLine(string.Join(" ", regexMatches);
