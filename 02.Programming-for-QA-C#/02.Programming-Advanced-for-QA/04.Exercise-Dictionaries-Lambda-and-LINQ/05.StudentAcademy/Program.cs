498098ea4133d16526a03f7580eb48f1cf3e3d6e
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-04-24 17:46:39 +0300

    Add files via upload

---
+++ 02.Programming-for-QA-C#/02.Programming-Advanced-for-QA/04.Exercise-Dictionaries-Lambda-and-LINQ/05.StudentAcademy/Program.cs
@@ -0,0 +1,30 @@
+﻿
+Dictionary<string, List<double>> students = new();
+
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    string name = Console.ReadLine();
+    double grade = double.Parse(Console.ReadLine());
+
+    if (!students.ContainsKey(name))
+    {
+        students.Add(name, new List<double>());
+    }
+
+    students[name].Add(grade);
+}
+
+// ако искаме да филтрираме речника преди foreach, но губим данни в оригиналния речник
+//students = students.Where(s => s.Value.Average() >= 4.50).ToDictionary(t => t.Key, t => t.Value);
+
+
+// филтрираме речника само за foreach, без да губим данни в оригиналния речник
+foreach (var kvp in students.Where(s => s.Value.Average() >= 4.50))
+{
+    string studentName = kvp.Key;
+    double averageGrade = kvp.Value.Average();
+
+    Console.WriteLine($"{studentName} -> {averageGrade:F2}");
+}
