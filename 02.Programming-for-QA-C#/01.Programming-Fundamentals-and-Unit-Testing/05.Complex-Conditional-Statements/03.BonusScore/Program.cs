6485a67e98b6078028146fbcc62e4ba15d1c848f
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-03 18:49:34 +0300

    Add files via upload

---
+++ 02.Programming-for-QA-C#/01.Programming-Fundamentals-and-Unit-Testing/05.Complex-Conditional-Statements/03.BonusScore/Program.cs
@@ -0,0 +1,20 @@
+﻿// Input
+int points = int.Parse(Console.ReadLine());
+
+// Calculations
+if (points >= 0 && points <= 3)
+{
+    points = points + 5;
+    //points += 5; по-кратък запис на горния ред
+}
+else if (points >= 4 && points <= 6)
+{
+    points = points + 15;
+}
+else if (points >= 7 && points <= 9)
+{
+    points = points + 20;
+}
+
+// Output
+Console.WriteLine(points);
