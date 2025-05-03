3f6ed3d3dadf89858db4295b742afb8a79c78fcc
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-03 19:31:16 +0300

    Add files via upload

---
+++ 02.Programming-for-QA-C#/01.Programming-Fundamentals-and-Unit-Testing/10.Nested-Loops/08.PrimeNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿int start = int.Parse(Console.ReadLine());
+int end = int.Parse(Console.ReadLine());
+
+for (int num = start; num <= end; num++)
+{
+    // брояч: едно число дали се дели на повече от две числа без остатък
+    int primeCounter = 0;
+
+    for (int i = 1; i <= num; i++)
+    {
+        // проверяваме дали числото се дели на друго число без остатък
+        if (num % i == 0)
+        {
+            primeCounter++;
+        }
+    }
+
+    // ако бороята е останал на 2, значи числото е просто и го изписваме на конзолата
+    if (primeCounter == 2)
+    {
+        Console.Write($"{num} ");
+    }
+}
