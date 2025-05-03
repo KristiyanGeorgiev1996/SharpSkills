cf012bd2c40eb0c9303062fc81c9148b6b960d9b
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-03 19:35:23 +0300

    Add files via upload

---
+++ 02.Programming-for-QA-C#/01.Programming-Fundamentals-and-Unit-Testing/11.Methods/03.PrintingTriangle/Program.cs
@@ -0,0 +1,29 @@
+﻿
+int n = int.Parse(Console.ReadLine());
+
+PrintTriange(n);
+
+static void PrintTriange(int n)
+{
+    // прав цикъл за първата част на триъгълника
+    for (int row = 1; row < n; row++)
+    {
+        PrintLine(1, row);
+    }
+
+    // обърнат цикъл за втората част на триъгълника
+    for (int row = n; row >= 1; row--)
+    {
+        PrintLine(1, row);
+    }
+}
+
+// метод който принтира само един ред от триъгълника
+static void PrintLine(int start, int end)
+{
+    for (int i = start; i <= end; i++)
+    {
+        Console.Write($"{i} ");
+    }
+    Console.WriteLine();
+}
