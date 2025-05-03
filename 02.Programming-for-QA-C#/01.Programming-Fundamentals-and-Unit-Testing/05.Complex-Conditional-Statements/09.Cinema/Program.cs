1d6af774b3509cd258fec671f0d7784a47b875f3
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-03 18:51:23 +0300

    Add files via upload

---
+++ 02.Programming-for-QA-C#/01.Programming-Fundamentals-and-Unit-Testing/05.Complex-Conditional-Statements/09.Cinema/Program.cs
@@ -0,0 +1,20 @@
+﻿// Input
+string typeOfMovie = Console.ReadLine();
+int countOfRows = int.Parse(Console.ReadLine());
+int countOfSeatsPerRow = int.Parse(Console.ReadLine());
+
+// Calculations
+int allSeatsInTheater = countOfRows * countOfSeatsPerRow;
+double priceForOneTicket = 0;
+
+switch (typeOfMovie)
+{
+    case "Premiere": priceForOneTicket = 12; break;
+    case "Normal": priceForOneTicket = 7.50; break;
+    case "Discount": priceForOneTicket = 5; break;
+}
+
+double finalPrice = allSeatsInTheater * priceForOneTicket;
+
+// Output
+Console.WriteLine($"{finalPrice:F2}");
