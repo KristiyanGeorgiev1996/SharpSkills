a8b60915e81696198bdd61b0e13e38e7f58cf7c4
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-03 19:11:09 +0300

    Add files via upload

---
+++ 02.Programming-for-QA-C#/01.Programming-Fundamentals-and-Unit-Testing/07.For-Loop/01.NumbersInRange/Program.cs
@@ -0,0 +1,11 @@
+﻿// Input
+
+int start = int.Parse(Console.ReadLine());
+int end = int.Parse(Console.ReadLine());
+
+// Output
+
+for (int i = start; i <= end; i++)
+{
+    Console.WriteLine(i);
+}
