c7995f5bcea4b765c0ce17ea52a88f6c22a67d58
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-03 18:59:08 +0300

    Add files via upload

---
+++ 02.Programming-for-QA-C#/01.Programming-Fundamentals-and-Unit-Testing/06.Exercise-Conditional-Statements/09.SumSeconds/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _09.SumSeconds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstTime = int.Parse(Console.ReadLine()); // 15
+            int secondTime = int.Parse(Console.ReadLine()); // 20
+            int thirdTime = int.Parse(Console.ReadLine()); // 30
+
+            int totalTimeInSeconds = firstTime + secondTime + thirdTime; // 65
+
+            int minutes = totalTimeInSeconds / 60;
+            int seconds = totalTimeInSeconds % 60;
+
+            Console.WriteLine($"{minutes}:{seconds:D2}");
+        }
+    }
+}
