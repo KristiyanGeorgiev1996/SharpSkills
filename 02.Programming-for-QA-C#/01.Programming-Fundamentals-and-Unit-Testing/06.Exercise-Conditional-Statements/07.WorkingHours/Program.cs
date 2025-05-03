1e184781a8341309c4ba61b81d0c59a9a42abbc9
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-03 18:58:28 +0300

    Add files via upload

---
+++ 02.Programming-for-QA-C#/01.Programming-Fundamentals-and-Unit-Testing/06.Exercise-Conditional-Statements/07.WorkingHours/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _07.WorkingHours
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = int.Parse(Console.ReadLine());
+            string day = Console.ReadLine();
+
+            bool workingDay = day != "Sunday";
+            bool workingHour = hour >= 10 && hour <= 18;
+            if (workingDay && workingHour)
+            {
+                Console.WriteLine("open");
+            }
+            else
+            {
+                Console.WriteLine("closed");
+            }
+        }
+    }
+}
