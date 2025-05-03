dabff1d84916f24fb9dbf38f45e07d466106bf55
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-03 18:57:12 +0300

    Add files via upload

---
+++ 02.Programming-for-QA-C#/01.Programming-Fundamentals-and-Unit-Testing/06.Exercise-Conditional-Statements/03.BiggestOfFiveNumbers/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _03.BiggestOfFiveNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int d = int.Parse(Console.ReadLine());
+            int e = int.Parse(Console.ReadLine());
+
+            int biggestNumber = int.MinValue; // -21123123
+            if (a > biggestNumber)
+            {
+                biggestNumber = a;
+            }
+            if (b > biggestNumber)
+            {
+                biggestNumber = b;
+            }
+            if (c > biggestNumber)
+            {
+                biggestNumber = c;
+            }
+            if (d > biggestNumber)
+            {
+                biggestNumber = d;
+            }
+            if (e > biggestNumber)
+            {
+                biggestNumber = e;
+            }
+
+            Console.WriteLine(biggestNumber);
+        }
+    }
+}
