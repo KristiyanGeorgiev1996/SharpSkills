fe8c5dff368ea188e3d81778c39f5adcff3dc98f
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-03 18:50:47 +0300

    Add files via upload

---
+++ 02.Programming-for-QA-C#/01.Programming-Fundamentals-and-Unit-Testing/05.Complex-Conditional-Statements/07.VowelОrConsonant/Program.cs
@@ -0,0 +1,21 @@
+﻿// Input
+char letter = char.Parse(Console.ReadLine());
+
+// Output
+switch (letter)
+{
+    case 'A':
+    case 'a':
+    case 'E':
+    case 'e':
+    case 'I':
+    case 'i':
+    case 'O':
+    case 'o':
+    case 'U':
+    case 'u':
+        Console.WriteLine("Vowel"); break;
+    default:
+        Console.WriteLine("Consonant");
+        break;
+}
