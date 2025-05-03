19370ddde98938de7780bc9da219582c6d596234
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-03 18:57:30 +0300

    Add files via upload

---
+++ 02.Programming-for-QA-C#/01.Programming-Fundamentals-and-Unit-Testing/06.Exercise-Conditional-Statements/04.NumberOperations/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _04.NumberOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            string @operator = Console.ReadLine();
+
+            if (@operator == "+")
+            {
+                Console.WriteLine($"{a} {@operator} {b} = {(a + b):F2}");
+            }
+            else if (@operator == "-")
+            {
+                Console.WriteLine($"{a} {@operator} {b} = {(a - b):F2}");
+            }
+            else if (@operator == "*")
+            {
+                Console.WriteLine($"{a} {@operator} {b} = {(a * b):F2}");
+            }
+            else
+            {
+                Console.WriteLine($"{a} {@operator} {b} = {(a / b):F2}");
+            }
+        }
+    }
+}
