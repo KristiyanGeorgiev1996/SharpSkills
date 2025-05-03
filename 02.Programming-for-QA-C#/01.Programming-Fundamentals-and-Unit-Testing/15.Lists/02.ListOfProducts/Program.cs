0fa153f344719873701fb997744016690575dc64
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-03 19:54:57 +0300

    Add files via upload

---
+++ 02.Programming-for-QA-C#/01.Programming-Fundamentals-and-Unit-Testing/15.Lists/02.ListOfProducts/Program.cs
@@ -0,0 +1,27 @@
+﻿
+int n = int.Parse(Console.ReadLine());
+
+List<string> listOfProducts = new List<string>();
+
+for (int i = 0; i < n; i++)
+{
+    string product = Console.ReadLine();
+
+    listOfProducts.Add(product);
+}
+
+listOfProducts.Sort();
+
+// Пешачатане на листа с for цикъл
+for (int i = 0; i < listOfProducts.Count; i++)
+{
+    Console.WriteLine($"{i + 1}.{listOfProducts[i]}");
+}
+
+// Пешачатане на листа с foreach цикъл
+int counter = 1;
+foreach (string product in listOfProducts)
+{
+    Console.WriteLine($"{counter}.{product}");
+    counter++;
+}
