5a3dba57a2c7ebb08ac902892b821f8c7b34b77b
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-03 19:17:19 +0300

    Add files via upload

---
+++ 02.Programming-for-QA-C#/01.Programming-Fundamentals-and-Unit-Testing/08.While-Loop/04.Sequence2k+1/Program.cs
@@ -0,0 +1,18 @@
+﻿
+int maxNum = int.Parse(Console.ReadLine());
+
+int k = 1;
+
+while (k <= maxNum)
+{
+    Console.WriteLine(k);
+
+    k = k * 2 + 1;
+}
+
+// Решемие на задачата с for цикъл:
+
+//for (int k = 1; k <= maxNum; k = k * 2 + 1)
+//{
+//    Console.WriteLine(k);
+//}
