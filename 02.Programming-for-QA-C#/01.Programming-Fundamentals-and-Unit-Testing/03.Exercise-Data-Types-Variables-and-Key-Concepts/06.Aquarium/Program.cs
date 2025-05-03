829832016b436541f477ee0e3452f0a3d78d0909
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-03 18:37:16 +0300

    Add files via upload

---
+++ 02.Programming-for-QA-C#/01.Programming-Fundamentals-and-Unit-Testing/03.Exercise-Data-Types-Variables-and-Key-Concepts/06.Aquarium/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _06.Aquarium
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+
+            string b = Console.ReadLine();
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            double decorationPercetange = double.Parse(Console.ReadLine());
+
+            double aquariumVolumeInCm = length * width * height;
+            double aquariumVolumeInLiters = aquariumVolumeInCm * 0.001;
+
+            //double decorationPercentageVolume = aquariumVolumeInLiters * (decorationPercetange / 100);
+            //double result = aquariumVolumeInLiters - decorationPercentageVolume;
+
+            double result = aquariumVolumeInLiters * (decorationPercetange / 1000);
+            Console.WriteLine($"{result:F2}");
+        }
+    }
+}
