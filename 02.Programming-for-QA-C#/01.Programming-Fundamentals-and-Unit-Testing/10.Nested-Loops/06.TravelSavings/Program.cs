8afcf9538a32fd89cb71bfac5542cddc3dc8e4de
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-03 19:30:40 +0300

    Add files via upload

---
+++ 02.Programming-for-QA-C#/01.Programming-Fundamentals-and-Unit-Testing/10.Nested-Loops/06.TravelSavings/Program.cs
@@ -0,0 +1,25 @@
+﻿
+string destination = Console.ReadLine();
+
+while (destination != "End")
+{
+    // прочитам необходимата сума за дестинацията
+    double neededMoney = double.Parse(Console.ReadLine());
+    double savedMoney = 0;
+
+    // четем пари от конзолата докато не съберем необходимата сума
+    while (savedMoney < neededMoney)
+    {
+        double currentMoney = double.Parse(Console.ReadLine());
+        // прибавяме текущите пари към спестените
+        savedMoney += currentMoney;
+
+        Console.WriteLine($"Collected: {savedMoney:F2}");
+    }
+
+    // имаме достатъчно пати и изписваме, че заминаваме за дестинацията
+    Console.WriteLine($"Going to {destination}!");
+
+    // четем нова дестинация
+    destination = Console.ReadLine();
+}
