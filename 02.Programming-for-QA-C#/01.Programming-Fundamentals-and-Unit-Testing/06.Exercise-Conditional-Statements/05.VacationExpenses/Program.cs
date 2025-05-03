437b7d1a97b64c7780e41c0e47ecd984efba5115
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-03 18:57:48 +0300

    Add files via upload

---
+++ 02.Programming-for-QA-C#/01.Programming-Fundamentals-and-Unit-Testing/06.Exercise-Conditional-Statements/05.VacationExpenses/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace _05.VacationExpenses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string season = Console.ReadLine();
+            string accomodationType = Console.ReadLine();
+            int days = int.Parse(Console.ReadLine());
+
+            int price = 0;
+            double multiplier = 1;
+            if (season == "Spring") // 20 discount
+            {
+                multiplier = 0.8;
+                if (accomodationType == "Hotel")
+                {
+                    price = 30;
+                }
+                else // Camping
+                {
+                    price = 10;
+                }
+            }
+            else if (season == "Summer") // 0% disount
+            {
+                if (accomodationType == "Hotel")
+                {
+                    price = 50;
+                }
+                else //Camping
+                {
+                    price = 30;
+                }
+            }
+            else if (season == "Autumn") // 30% disount
+            {
+                multiplier = 0.7;
+                if (accomodationType == "Hotel")
+                {
+                    price = 20;
+                }
+                else //Camping
+                {
+                    price = 15;
+                }
+            }
+            else // Winter // 10% disount
+            {
+                multiplier = 0.9;
+                if (accomodationType == "Hotel")
+                {
+                    price = 40;
+                }
+                else //Camping
+                {
+                    price = 10;
+                }
+            }
+
+            // output
+            double totalPrice = (price * days) * multiplier;
+            Console.WriteLine($"{totalPrice:F2}");
+        }
+    }
+}
