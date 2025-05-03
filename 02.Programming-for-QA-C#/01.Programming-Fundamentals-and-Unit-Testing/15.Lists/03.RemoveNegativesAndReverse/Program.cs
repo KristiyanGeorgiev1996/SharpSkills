ebe5b27bd6d22277b5ae9369e83a2c0cbb71726f
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-03 19:55:17 +0300

    Add files via upload

---
+++ 02.Programming-for-QA-C#/01.Programming-Fundamentals-and-Unit-Testing/15.Lists/03.RemoveNegativesAndReverse/Program.cs
@@ -0,0 +1,26 @@
+﻿
+List<int> numbers = Console.ReadLine()
+                            .Split(' ')
+                            .Select(int.Parse)
+                            .ToList();
+
+for (int i = 0; i < numbers.Count; i++)
+{
+    if (numbers[i] < 0)
+    {
+        numbers.RemoveAt(i); // изтриваме елемент на даден индекс
+        i--; // намаляме индеска, за да не изпуснем елемент
+    }
+}
+
+// обръщаме елементите в листа в обратен ред (несортирани!!!)
+numbers.Reverse();
+
+if (numbers.Count == 0)
+{
+    Console.WriteLine("empty");
+}
+else
+{
+    Console.WriteLine(string.Join(" ", numbers));
+}
