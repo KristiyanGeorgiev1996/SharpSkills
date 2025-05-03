ec82f41225bfa1e582b66308439b84544fd4ea4e
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-03 18:31:28 +0300

    Add files via upload

---
+++ 02.Programming-for-QA-C#/01.Programming-Fundamentals-and-Unit-Testing/02.Key-Concepts/05.DepositCalculator/Program.cs
@@ -0,0 +1,16 @@
+﻿
+// Input
+
+double depositAmount = double.Parse(Console.ReadLine());
+int countOfMonts = int.Parse(Console.ReadLine());
+double interestPercentPerYear = double.Parse(Console.ReadLine());
+
+// Calculate
+
+double yearIncome = depositAmount * interestPercentPerYear / 100;
+double monthIncome = yearIncome / 12;
+double finalAmount = depositAmount + (countOfMonts * monthIncome);
+
+// Output
+
+Console.WriteLine($"{finalAmount:F2}");
