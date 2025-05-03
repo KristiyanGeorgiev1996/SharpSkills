2484d633a86d898d87c440ce5fc7c046b595ef0d
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-03 18:58:49 +0300

    Add files via upload

---
+++ 02.Programming-for-QA-C#/01.Programming-Fundamentals-and-Unit-Testing/06.Exercise-Conditional-Statements/08.FruitOrVegetable/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _08.FruitOrVegetable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string productType = Console.ReadLine();
+            switch (productType)
+            {
+                case "banana" or "apple" or "kiwi" or "cherry" or "lemon":
+                    Console.WriteLine("fruit");
+                    break;
+                case "cucumber" or "pepper" or "carrot":
+                    Console.WriteLine("vegetable");
+                    break;
+                default:
+                    Console.WriteLine("unknown");
+                    break;
+            }
+        }
+    }
+}
