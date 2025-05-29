983e816dde1add63a3fe6b94e0a3023b7056e9b5
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-29 17:59:00 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/01.Basic-Syntax-Conditional-Statements-and-Loops/10.MultiplicationTable/Program.cs
@@ -0,0 +1,10 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+int times = 0;
+
+for (int i = 1; i <= 10; i++)
+{
+    times += 1;
+    double product = n * times;
+    Console.WriteLine($"{n} X {times} = {product}");
+}
