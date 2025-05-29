b94c8f6ee622e870347b978e4b1feec2c9524518
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-29 17:59:21 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/01.Basic-Syntax-Conditional-Statements-and-Loops/11.MultiplicationTable2.0/Program.cs
@@ -0,0 +1,20 @@
+﻿int firstNum = int.Parse(Console.ReadLine());
+int secondNum = int.Parse(Console.ReadLine());
+
+double product = 0;
+int times = secondNum;
+
+if (secondNum <= 10)
+{
+    for (int i = secondNum; i <= 10; i++)
+    {
+        product = firstNum * times;
+        Console.WriteLine($"{firstNum} X {times} = {product}");
+        times += 1;
+    }
+}
+else
+{
+    product = firstNum * secondNum;
+    Console.WriteLine($"{firstNum} X {secondNum} = {product}");
+}
