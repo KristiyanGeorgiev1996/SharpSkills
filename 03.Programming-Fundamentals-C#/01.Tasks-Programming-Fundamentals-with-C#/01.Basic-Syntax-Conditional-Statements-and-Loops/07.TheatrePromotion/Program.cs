9c9db7a1150c1d6f2aa2ee3484908865f30ebc46
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-29 17:58:00 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/01.Basic-Syntax-Conditional-Statements-and-Loops/07.TheatrePromotion/Program.cs
@@ -0,0 +1,64 @@
+﻿string day = Console.ReadLine();
+int ages = int.Parse(Console.ReadLine());
+
+if (day == "Weekday")
+{
+    if (ages >= 0 && ages <= 18)
+    {
+        Console.WriteLine("12$");
+    }
+    else if (ages >= 19 && ages <= 64)
+    {
+        Console.WriteLine("18$");
+    }
+    else if (ages >= 65 && ages <= 122)
+    {
+        Console.WriteLine("12$");
+    }
+    else
+    {
+        Console.WriteLine("Error!");
+    }
+}
+else if (day == "Weekend")
+{
+    if (ages >= 0 && ages <= 18)
+    {
+        Console.WriteLine("15$");
+    }
+    else if (ages >= 19 && ages <= 64)
+    {
+        Console.WriteLine("20$");
+    }
+    else if (ages >= 65 && ages <= 122)
+    {
+        Console.WriteLine("15$");
+    }
+    else
+    {
+        Console.WriteLine("Error!");
+    }
+}
+else if (day == "Holiday")
+{
+    if (ages >= 0 && ages <= 18)
+    {
+        Console.WriteLine("5$");
+    }
+    else if (ages >= 19 && ages <= 64)
+    {
+        Console.WriteLine("12$");
+    }
+    else if (ages >= 65 && ages <= 122)
+    {
+        Console.WriteLine("10$");
+    }
+    else
+    {
+        Console.WriteLine("Error!");
+    }
+}
+else
+{
+    Console.WriteLine("Error!");
+}
