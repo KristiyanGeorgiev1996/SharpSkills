01eada336a68d725977af10ef477f8007f628714
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-29 17:57:20 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/01.Basic-Syntax-Conditional-Statements-and-Loops/05.MonthPrinter/Program.cs
@@ -0,0 +1,54 @@
+﻿int numberOfMonth = int.Parse(Console.ReadLine());
+
+if (numberOfMonth == 1)
+{
+    Console.WriteLine("January");
+}
+else if (numberOfMonth == 2)
+{
+    Console.WriteLine("February");
+}
+else if (numberOfMonth == 3)
+{
+    Console.WriteLine("March");
+}
+else if (numberOfMonth == 4)
+{
+    Console.WriteLine("April");
+}
+else if (numberOfMonth == 5)
+{
+    Console.WriteLine("May");
+}
+else if (numberOfMonth == 6)
+{
+    Console.WriteLine("June");
+}
+else if (numberOfMonth == 7)
+{
+    Console.WriteLine("Jule");
+}
+else if (numberOfMonth == 8)
+{
+    Console.WriteLine("August");
+}
+else if (numberOfMonth == 9)
+{
+    Console.WriteLine("September");
+}
+else if (numberOfMonth == 10)
+{
+    Console.WriteLine("October");
+}
+else if (numberOfMonth == 11)
+{
+    Console.WriteLine("November");
+}
+else if (numberOfMonth == 12)
+{
+    Console.WriteLine("December");
+}
+else
+{
+    Console.WriteLine("Error!");
+}
