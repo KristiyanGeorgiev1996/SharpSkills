495fd50bf39f19bbb39aa321f3f40657d1a77113
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-30 18:33:13 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/07.Methods/11.MathOperations/Program.cs
@@ -0,0 +1,27 @@
+﻿int firstNumber = int.Parse(Console.ReadLine());
+string calculation = Console.ReadLine();
+int secNumber = int.Parse(Console.ReadLine());
+
+int sum = 0;
+MathOperations(firstNumber, secNumber);
+
+void MathOperations(int firstNumber, int secNumber)
+{
+    if (calculation == "*")
+    {
+        sum = firstNumber * secNumber;
+    }
+    else if (calculation == "+")
+    {
+        sum = firstNumber + secNumber;
+    }
+    else if (calculation == "/")
+    {
+        sum = firstNumber / secNumber;
+    }
+    else if (calculation == "-")
+    {
+        sum = firstNumber - secNumber;
+    }
+}
+Console.WriteLine(sum);
