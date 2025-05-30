c148b7539d4097b7ad3742b5ced361cd61a3c0a5
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-30 17:31:28 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/04.Exercise-Data-Types-and-Variables/15.RefactoringPrimeChecker/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        //Data Types and Variables - More Exercise - 04. Refactoring: Prime Checker
+
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 2; i <= n; i++)
+        {
+            bool isPrime = true;
+            for (int devider = 2; devider < i; devider++)
+            {
+                if (i % devider == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime)
+            {
+                Console.WriteLine($"{i} -> true");
+            }
+            else
+            {
+                Console.WriteLine($"{i} -> false");
+            }
+        }
+    }
+}
