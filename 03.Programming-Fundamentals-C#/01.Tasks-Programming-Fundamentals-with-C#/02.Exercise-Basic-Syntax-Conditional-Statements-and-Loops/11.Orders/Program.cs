1571b74f183c7080a90ab44e219eaf6f2b055c04
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-30 17:02:34 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/02.Exercise-Basic-Syntax-Conditional-Statements-and-Loops/11.Orders/Program.cs
@@ -0,0 +1,14 @@
+﻿double poruhki = double.Parse(Console.ReadLine());
+double allSum = 0;
+for (int i = 0; i < poruhki; i++)
+{
+    double priceforCapsule = double.Parse(Console.ReadLine());
+    double days = double.Parse(Console.ReadLine());
+    double capsuleCount = double.Parse(Console.ReadLine());
+
+    double totalSum = (days * capsuleCount) * priceforCapsule;
+    allSum += totalSum;
+    Console.WriteLine($"The price for the coffee is: ${totalSum:F2}");
+}
+
+Console.WriteLine($"Total: ${allSum:F2}");
