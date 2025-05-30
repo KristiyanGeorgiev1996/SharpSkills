cc9e4f109e0c41f12329c7f3881ec49692ed1159
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-30 17:38:40 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/05.Arrays/07.EqualArrays/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] array1 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int[] array2 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int sumOfAllNumbers = 0;
+int n = 0;
+
+for (int i = 0; i < array1.Length; i++)
+{
+    if (array1[i] != array2[i])
+    {
+        Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+        break;
+    }
+    else
+    {
+        n++;
+        sumOfAllNumbers += array1[i];
+    }
+}
+if (n == array1.Length)
+{
+    Console.WriteLine($"Arrays are identical. Sum: {sumOfAllNumbers}");
+}
+
+
