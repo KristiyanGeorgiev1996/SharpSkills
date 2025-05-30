da3509c212fad1f5162e38dd20a4e579ac6b94bd
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-30 18:12:43 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/07.Methods/07.RepeatString/Program.cs
@@ -0,0 +1,12 @@
+﻿string text = Console.ReadLine();
+int count = int.Parse(Console.ReadLine());
+
+Rtring(text, count);
+
+void Rtring(string text, int count)
+{
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{text}");
+    }
+}
