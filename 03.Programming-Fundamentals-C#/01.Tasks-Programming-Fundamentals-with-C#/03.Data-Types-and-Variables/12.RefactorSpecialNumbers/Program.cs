9d379aebf6c86fc088370149287fbad25e78456b
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-30 17:15:15 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/03.Data-Types-and-Variables/12.RefactorSpecialNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+{
+    int currentNumber = i;
+    int digitSum = 0;
+    while (currentNumber > 0)
+    {
+        digitSum += currentNumber % 10;
+        currentNumber /= 10;
+    }
+
+    bool isSpecial = false;
+    if (digitSum == 5 || digitSum == 7 || digitSum == 11)
+    {
+        isSpecial = true;
+    }
+    Console.WriteLine($"{i} -> {isSpecial}");
+}
