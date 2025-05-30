07de1debbb3bcc0bb5ad4410d8a42812a8d0a89e
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-30 18:11:37 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/07.Methods/03.Calculations/Program.cs
@@ -0,0 +1,31 @@
+﻿string mathOperacion = Console.ReadLine();
+int firstNumber = int.Parse(Console.ReadLine());
+int secNumber = int.Parse(Console.ReadLine());
+
+int sum = 0;
+
+MathOperation(mathOperacion);
+
+void MathOperation(string mathOperation)
+{
+    if (mathOperation == "add")
+    {
+        sum = firstNumber + secNumber;
+        Console.WriteLine(sum);
+    }
+    else if (mathOperation == "divide")
+    {
+        sum = firstNumber / secNumber;
+        Console.WriteLine(sum);
+    }
+    else if (mathOperation == "multiply")
+    {
+        sum = firstNumber * secNumber;
+        Console.WriteLine(sum);
+    }
+    else if (mathOperation == "subtract")
+    {
+        sum = firstNumber - secNumber;
+        Console.WriteLine(sum);
+    }
+}
