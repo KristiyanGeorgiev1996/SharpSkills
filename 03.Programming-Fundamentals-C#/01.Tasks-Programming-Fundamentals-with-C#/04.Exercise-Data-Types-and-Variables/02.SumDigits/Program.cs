9b8ddc3c5c12842e0c1f5d8a8ba992a402be4431
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-30 17:27:50 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/04.Exercise-Data-Types-and-Variables/02.SumDigits/Program.cs
@@ -0,0 +1,13 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+double N = 0;
+double sumOfNumber = 0;
+
+sumOfNumber = 0;
+while (number != 0)
+{
+    sumOfNumber += number % 10;
+    number /= 10;
+
+}
+Console.WriteLine(sumOfNumber);
