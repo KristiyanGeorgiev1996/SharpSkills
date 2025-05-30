e0a1dc6eefbca433a6ea03aec48c99c856ea6c73
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-30 17:14:45 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/03.Data-Types-and-Variables/11.RefactorVolumeOfPyramid/Program.cs
@@ -0,0 +1,11 @@
+﻿decimal lenght = decimal.Parse(Console.ReadLine());
+decimal width = decimal.Parse(Console.ReadLine());
+decimal height = decimal.Parse(Console.ReadLine());
+
+decimal V1 = lenght * width * height;
+decimal V = V1 / 3;
+
+Console.Write("Length:");
+Console.Write(" Width:");
+Console.Write(" Height:");
+Console.Write($" Pyramid Volume: {V:f2}");
