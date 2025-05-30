87621db612d8bdac3139c25bdf0e682c146faba7
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-30 17:29:13 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/04.Exercise-Data-Types-and-Variables/07.WaterOverflow/Program.cs
@@ -0,0 +1,20 @@
+﻿int n = int.Parse(Console.ReadLine());
+int allLiters = 0;
+
+for (int i = 1; i <= n; i++)
+{
+    int liters = int.Parse(Console.ReadLine());
+    if (liters > 255)
+    {
+        Console.WriteLine("Insufficient capacity!");
+    }
+    else if (liters + allLiters <= 255)
+    {
+        allLiters += liters;
+    }
+    else if (liters + allLiters > 255)
+    {
+        Console.WriteLine("Insufficient capacity!");
+    }
+}
+Console.WriteLine(allLiters);
