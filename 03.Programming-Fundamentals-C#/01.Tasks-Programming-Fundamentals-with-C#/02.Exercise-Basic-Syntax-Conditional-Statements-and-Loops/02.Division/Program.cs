636b7e36587bb991a4c39a1a6d8792b0a449f60f
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-30 16:59:30 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/02.Exercise-Basic-Syntax-Conditional-Statements-and-Loops/02.Division/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.Design;
+
+int number = int.Parse(Console.ReadLine());
+
+////2 3 6 7 10
+
+int chislo = 0;
+
+if (number % 10 == 0)
+{
+    chislo = 10;
+    Console.WriteLine($"The number is divisible by {chislo}");
+}
+else if (number % 7 == 0)
+{
+    chislo = 7;
+    Console.WriteLine($"The number is divisible by {chislo}");
+}
+else if (number % 6 == 0)
+{
+    chislo = 6;
+    Console.WriteLine($"The number is divisible by {chislo}");
+}
+else if (number % 3 == 0)
+{
+    chislo = 3;
+    Console.WriteLine($"The number is divisible by {chislo}");
+}
+else if (number % 2 == 0)
+{
+    chislo = 2;
+    Console.WriteLine($"The number is divisible by {chislo}");
+}
+if (chislo == 0)
+{
+    Console.WriteLine("Not divisible");
+}
