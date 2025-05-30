c57ac8440f3d13e0a3ce183a7cd65f9f7708cbb9
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-30 18:11:05 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/07.Methods/01.SignOfIntegerNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿int number = int.Parse(Console.ReadLine());
+WhatIsNumber(number);
+void WhatIsNumber(int number)
+{
+    if (number < 0)
+    {
+        Console.WriteLine($"The number {number} is negative. ");
+    }
+    else if (number > 0)
+    {
+        Console.WriteLine($"The number {number} is positive. ");
+    }
+    else
+    {
+        Console.WriteLine($"The number {number} is zero. ");
+    }
+}
+
