b90ffe37ef3e20941344995217c073edafc01f79
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-30 17:01:43 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/02.Exercise-Basic-Syntax-Conditional-Statements-and-Loops/08.TriangleOfNumbers/Program.cs
@@ -0,0 +1,10 @@
+﻿int x = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= x; i++)
+{
+    for (int j = 1; j < i; j++)
+    {
+        Console.Write($"{i} ");
+    }
+    Console.WriteLine($"{i} ");
+}
