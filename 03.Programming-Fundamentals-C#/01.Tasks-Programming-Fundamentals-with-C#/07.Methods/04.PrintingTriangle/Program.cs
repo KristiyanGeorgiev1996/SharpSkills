77e241deaa431453035f7ac0dca444d41cf8e7de
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-30 18:11:52 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/07.Methods/04.PrintingTriangle/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing;
+
+int n = int.Parse(Console.ReadLine());
+Rectangle(n);
+void Rectangle(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        Rectangle2(i + 1);
+    }
+    for (int i = 1; i < n; i++)
+    {
+        Rectangle2(n - i);
+    }
+}
+void Rectangle2(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        Console.Write($"{i + 1} ");
+    }
+    Console.WriteLine();
+}
