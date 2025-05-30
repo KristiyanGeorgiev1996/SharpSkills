b4e6e5152ae45d0a225bac7d5c02d43776857505
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-30 17:36:57 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/05.Arrays/01.DayOfWeek/Program.cs
@@ -0,0 +1,16 @@
+﻿string[] daysOfWeek = ("Monday Tuesday Wednesday Thursday Friday Saturday Sunday").Split();
+
+for (int i = 0; i < daysOfWeek.Length; i++)
+{
+    int n = int.Parse(Console.ReadLine());
+    if (n >= 1 && n <= 7)
+    {
+        Console.WriteLine(daysOfWeek[n - 1]);
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Invalid day!");
+        break;
+    }
+}
