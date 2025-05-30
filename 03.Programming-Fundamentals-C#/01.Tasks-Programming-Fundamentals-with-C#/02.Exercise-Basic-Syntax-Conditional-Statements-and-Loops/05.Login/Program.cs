07b53a9298e6d552858e91b8a656ee6aa07991ec
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-30 17:00:41 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/02.Exercise-Basic-Syntax-Conditional-Statements-and-Loops/05.Login/Program.cs
@@ -0,0 +1,31 @@
+﻿string username = Console.ReadLine();
+
+
+char[] stringaArra = username.ToCharArray();
+Array.Reverse(stringaArra);
+string poswword = new string(stringaArra);
+int opit = 4;
+
+while (opit > 0)
+{
+    opit -= 1;
+    string typePass = Console.ReadLine();
+
+    if (typePass == poswword)
+    {
+        Console.WriteLine($"User {username} logged in.");
+        break;
+
+    }
+    else
+    {
+        if (opit == 0)
+        {
+            Console.WriteLine($"User {username} blocked!");
+        }
+        else
+        {
+            Console.WriteLine($"Incorrect password. Try again.");
+        }
+    }
+}
