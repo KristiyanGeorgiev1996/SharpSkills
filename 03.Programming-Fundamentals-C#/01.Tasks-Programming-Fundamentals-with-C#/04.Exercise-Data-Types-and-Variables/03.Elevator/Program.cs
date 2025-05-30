2f51a3a3c2fd23501df1bcccf828af658517349d
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-30 17:28:05 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/04.Exercise-Data-Types-and-Variables/03.Elevator/Program.cs
@@ -0,0 +1,20 @@
+﻿double peoples = double.Parse(Console.ReadLine());
+double maxPeople = double.Parse(Console.ReadLine());
+
+double courses = 0;
+double oneMoreCourse;
+
+double course = peoples / maxPeople;
+
+if (peoples % maxPeople == 0)
+{
+    courses += course;
+}
+else if (peoples % maxPeople != 0)
+{
+    oneMoreCourse = course;
+    double ceiling = Math.Ceiling(oneMoreCourse);
+    courses += ceiling;
+}
+
+Console.WriteLine(courses);
