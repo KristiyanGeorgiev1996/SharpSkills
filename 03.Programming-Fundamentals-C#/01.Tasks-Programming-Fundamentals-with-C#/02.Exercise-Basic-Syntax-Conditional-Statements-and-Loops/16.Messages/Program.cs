d731f2d18a82f73cc08398df29df1f9365b9017f
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-30 17:04:05 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/02.Exercise-Basic-Syntax-Conditional-Statements-and-Loops/16.Messages/Program.cs
@@ -0,0 +1,122 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+{
+    int numbersOfWord = int.Parse(Console.ReadLine());
+
+    if (numbersOfWord == 2)
+    {
+        Console.Write("a");
+    }
+    else if (numbersOfWord == 22)
+    {
+        Console.Write("b");
+    }
+    else if (numbersOfWord == 222)
+    {
+        Console.Write("c");
+    }
+    else if (numbersOfWord == 3)
+    {
+        Console.Write("d");
+    }
+    else if (numbersOfWord == 33)
+    {
+        Console.Write("e");
+    }
+    else if (numbersOfWord == 333)
+    {
+        Console.Write("f");
+    }
+    else if (numbersOfWord == 4)
+    {
+        Console.Write("g");
+    }
+    else if (numbersOfWord == 44)
+    {
+        Console.Write("h");
+    }
+    else if (numbersOfWord == 444)
+    {
+        Console.Write("i");
+    }
+    else if (numbersOfWord == 5)
+    {
+        Console.Write("j");
+    }
+    else if (numbersOfWord == 55)
+    {
+        Console.Write("k");
+    }
+    else if (numbersOfWord == 555)
+    {
+        Console.Write("l");
+    }
+    else if (numbersOfWord == 6)
+    {
+        Console.Write("m");
+    }
+    else if (numbersOfWord == 66)
+    {
+        Console.Write("n");
+    }
+    else if (numbersOfWord == 666)
+    {
+        Console.Write("o");
+    }
+    else if (numbersOfWord == 7)
+    {
+        Console.Write("p");
+    }
+    else if (numbersOfWord == 77)
+    {
+        Console.Write("q");
+    }
+    else if (numbersOfWord == 777)
+    {
+        Console.Write("r");
+    }
+    else if (numbersOfWord == 7777)
+    {
+        Console.Write("s");
+    }
+    else if (numbersOfWord == 8)
+    {
+        Console.Write("t");
+    }
+    else if (numbersOfWord == 88)
+    {
+        Console.Write("u");
+    }
+    else if (numbersOfWord == 888)
+    {
+        Console.Write("v");
+    }
+    else if (numbersOfWord == 9)
+    {
+        Console.Write("w");
+    }
+    else if (numbersOfWord == 99)
+    {
+        Console.Write("x");
+    }
+    else if (numbersOfWord == 999)
+    {
+        Console.Write("y");
+    }
+    else if (numbersOfWord == 9999)
+    {
+        Console.Write("z");
+    }
+    else if (numbersOfWord == 0)
+    {
+        Console.Write(" ");
+    }
+
+
+    numbersOfWord--;
+    if (numbersOfWord == 0)
+    {
+        break;
+    }
+}
