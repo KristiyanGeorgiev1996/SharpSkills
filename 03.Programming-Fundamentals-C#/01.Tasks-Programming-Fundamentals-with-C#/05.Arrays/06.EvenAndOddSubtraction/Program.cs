31a5a30598e105c5d4f5175fe5feb42516d3d354
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-30 17:38:25 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/05.Arrays/06.EvenAndOddSubtraction/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int sumEven = 0;
+int sumOdd = 0;
+int result = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    int currentNumber = numbers[i];
+    if (currentNumber % 2 == 0)
+    {
+        sumEven += currentNumber;
+    }
+    else if (currentNumber % 2 == 1)
+    {
+        sumOdd += currentNumber;
+    }
+}
+
+result = sumEven - sumOdd;
+Console.WriteLine(result);
