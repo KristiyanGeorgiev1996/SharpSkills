c7a0b685fc48da66a9ea3ed9875c1d9ec583aaf0
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-30 17:49:29 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/06.Exercise-Arrays/07.MaxSequenceOfEqualElements/Program.cs
@@ -0,0 +1,32 @@
+﻿string[] symbol = Console.ReadLine().Split();
+
+int bestCount = 0;
+string bestSymbol = "";
+
+for (int i = symbol.Length - 1; i >= 0; i--)
+{
+    int count = 1;
+    for (int j = i - 1; j >= 0; j--)
+    {
+        if (symbol[i] == symbol[j])
+        {
+            count++;
+            if (bestCount <= count)
+            {
+                bestCount = count;
+                bestSymbol = symbol[i];
+            }
+
+        }
+        else
+        {
+            break;
+        }
+    }
+
+
+}
+for (int i = 0; i < bestCount; i++)
+{
+    Console.Write($"{bestSymbol} ");
+}
