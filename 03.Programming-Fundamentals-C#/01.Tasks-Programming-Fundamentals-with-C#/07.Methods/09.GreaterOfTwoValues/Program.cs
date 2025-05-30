e1936f4dc125ca69f7f3bf8ccf9690567e3d25a9
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-30 18:32:37 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/07.Methods/09.GreaterOfTwoValues/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _09._Greater_of_Two_Values
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputName = Console.ReadLine();
+            string name1 = Console.ReadLine();
+            string name2 = Console.ReadLine();
+
+            Console.WriteLine(GameMax(inputName, name1, name2));
+        }
+
+        static string GameMax(string inputName, string name1, string name2)
+        {
+            string result = "";
+
+            switch (inputName)
+            {
+                case "int":
+                    result = (Math.Max(int.Parse(name1), int.Parse(name2))).ToString();
+                    break;
+                case "char":
+                case "string":
+                    return name1[0] > name2[0] ? name1 : name2;
+                default:
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
