b1e0527bcd41ee616f9b752bc73130c72d43a1ea
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-30 18:12:09 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/07.Methods/05.Orders/Program.cs
@@ -0,0 +1,30 @@
+﻿string product = Console.ReadLine();
+int price = int.Parse(Console.ReadLine());
+
+double totalSum = 0;
+
+TotalPriceOfProducts(product, price);
+
+void TotalPriceOfProducts(string product, double price)
+{
+    if (product == "coffee")
+    {
+        totalSum = price * 1.50;
+        Console.WriteLine($"{totalSum:F2}");
+    }
+    else if (product == "water")
+    {
+        totalSum = price * 1.00;
+        Console.WriteLine($"{totalSum:F2}");
+    }
+    else if (product == "coke")
+    {
+        totalSum = price * 1.40;
+        Console.WriteLine($"{totalSum:F2}");
+    }
+    else if (product == "snacks")
+    {
+        totalSum = price * 2.00;
+        Console.WriteLine($"{totalSum:F2}");
+    }
+}
