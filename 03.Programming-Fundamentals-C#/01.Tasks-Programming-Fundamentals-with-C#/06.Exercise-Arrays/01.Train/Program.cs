282fc2b9c4e8cedf9f09bbb8a749bb58b8a1fcd8
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-30 17:47:51 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/06.Exercise-Arrays/01.Train/Program.cs
@@ -0,0 +1,22 @@
+﻿int n = int.Parse(Console.ReadLine());
+int[] array = new int[n];
+
+int sum = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = int.Parse(Console.ReadLine());
+    sum = sum + array[i];
+}
+for (int i = 0; i < array.Length; i++)
+{
+    if (i == array.Length - 1)
+    {
+        Console.WriteLine(array[i]);
+    }
+    else
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+Console.WriteLine(sum);
