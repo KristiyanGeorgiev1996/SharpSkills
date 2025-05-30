5ce013bd3b74cb6b945dae3d6cff228a99365d1f
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-30 17:00:21 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/02.Exercise-Basic-Syntax-Conditional-Statements-and-Loops/04.PrintAndSum/Program.cs
@@ -0,0 +1,12 @@
+﻿int number1 = int.Parse(Console.ReadLine());
+int number2 = int.Parse(Console.ReadLine());
+
+double allSum = 0;
+
+for (int i = number1; i <= number2; i++)
+{
+    Console.Write($"{i} ");
+    allSum += i;
+}
+Console.WriteLine();
+Console.WriteLine($"Sum: {allSum}");
