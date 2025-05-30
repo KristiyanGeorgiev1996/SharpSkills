e69b952fd977197dc049eef9b1b8bd71b28137bb
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-30 17:49:12 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/06.Exercise-Arrays/06.EqualSum/Program.cs
@@ -0,0 +1,31 @@
+﻿int[] values = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+bool isFound = false;
+
+for (int i = 0; i < values.Length; i++)
+{
+    int leftSum = 0;
+    for (int k = 0; k < i; k++)
+    {
+        leftSum += values[k];
+    }
+
+    int rightSum = 0;
+    for (int j = values.Length - 1; j > i; j--)
+    {
+        rightSum += values[j];
+    }
+
+    if (leftSum == rightSum && !isFound)
+    {
+        Console.WriteLine(i);
+        isFound = true;
+    }
+}
+
+if (!isFound)
+{
+    Console.WriteLine("no");
+}
