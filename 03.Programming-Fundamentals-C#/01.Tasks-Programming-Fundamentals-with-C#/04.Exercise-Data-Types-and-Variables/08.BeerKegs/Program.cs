fcce91f0e0cd86ea7df6c501fdd71344489c70ab
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-30 17:29:29 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/04.Exercise-Data-Types-and-Variables/08.BeerKegs/Program.cs
@@ -0,0 +1,20 @@
+﻿int countKegs = int.Parse(Console.ReadLine());
+
+string biggestModelName = "";
+double biggestKegObem = 0;
+
+for (int i = 1; i <= countKegs; i++)
+{
+    string model = Console.ReadLine();
+    double radius = double.Parse(Console.ReadLine());
+    int high = int.Parse(Console.ReadLine());
+
+    double volume = Math.PI * (Math.Pow(radius, 2)) * high;
+    if (biggestKegObem < volume)
+    {
+        biggestKegObem = volume;
+        biggestModelName = model;
+
+    }
+}
+Console.WriteLine(biggestModelName);
