cb9cb0ab1fd277d2a3fcabdbf0a327a75eea224e
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-30 17:01:08 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/02.Exercise-Basic-Syntax-Conditional-Statements-and-Loops/06.StrongNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+int nuberCopy = number;
+int factSum = 0;
+
+
+while (nuberCopy > 0)
+{
+    int digit = nuberCopy % 10;
+    nuberCopy /= 10;
+
+    int factorial = 1;
+    for (int i = 1; i <= digit; i++)
+    {
+        factorial *= i;
+    }
+
+    factSum += factorial;
+
+}
+string isStronngNumber = number == factSum ? "yes" : "no";
+Console.WriteLine(isStronngNumber);
+
+
