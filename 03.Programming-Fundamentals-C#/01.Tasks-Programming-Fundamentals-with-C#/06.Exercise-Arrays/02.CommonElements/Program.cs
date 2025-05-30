3426845d04e4dc38718da458e3db31e58c0323cf
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-30 17:48:07 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/06.Exercise-Arrays/02.CommonElements/Program.cs
@@ -0,0 +1,14 @@
+﻿string[] arr1 = Console.ReadLine().Split();
+string[] arr2 = Console.ReadLine().Split();
+
+for (int i = 0; i < arr2.Length; i++)
+{
+    string temp = arr2[i];
+    for (int j = 0; j < arr1.Length; j++)
+    {
+        if (arr1[j] == temp)
+        {
+            Console.Write($"{temp} ");
+        }
+    }
+}
