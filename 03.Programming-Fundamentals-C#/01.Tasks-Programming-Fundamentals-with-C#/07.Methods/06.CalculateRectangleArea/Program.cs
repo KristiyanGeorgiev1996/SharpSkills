47e154901ff73bcc5473af7772649a9cfee953a3
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-30 18:12:27 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/07.Methods/06.CalculateRectangleArea/Program.cs
@@ -0,0 +1,10 @@
+﻿int n = int.Parse(Console.ReadLine());
+int x = int.Parse(Console.ReadLine());
+int area = 0;
+RectangleArea(n, x);
+
+void RectangleArea(int n, int x)
+{
+    area = n * x;
+    Console.WriteLine(area);
+}
