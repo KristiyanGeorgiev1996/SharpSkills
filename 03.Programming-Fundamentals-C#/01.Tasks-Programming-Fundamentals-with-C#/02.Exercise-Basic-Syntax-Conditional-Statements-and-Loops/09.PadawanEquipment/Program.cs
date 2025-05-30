af2f404cc4fe4361dc58a54c89ebe98720b711cd
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-30 17:02:02 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/02.Exercise-Basic-Syntax-Conditional-Statements-and-Loops/09.PadawanEquipment/Program.cs
@@ -0,0 +1,29 @@
+﻿double money = double.Parse(Console.ReadLine());
+int students = int.Parse(Console.ReadLine());
+double swords = double.Parse(Console.ReadLine());
+double belt = double.Parse(Console.ReadLine());
+double robe = double.Parse(Console.ReadLine());
+
+double realSwords = students * 1.1;
+double realPriceSwords = Math.Ceiling(realSwords);
+
+double freeBelts = students / 6;
+double realNumOfBelts = students - freeBelts;
+double realBelts = Math.Floor(realNumOfBelts);
+
+double allPriceOfSwords = swords * realPriceSwords;
+double allPriceOfBelts = realBelts * robe;
+double allPriceOfRobe = students * belt;
+
+
+double allSum = allPriceOfBelts + allPriceOfRobe + allPriceOfSwords;
+
+if (allSum <= money)
+{
+    Console.WriteLine($"The money is enough - it would cost {allSum:F2}lv.");
+}
+else
+{
+    double needed = allSum - money;
+    Console.WriteLine($" John will need {needed:F2}lv more.");
+}
