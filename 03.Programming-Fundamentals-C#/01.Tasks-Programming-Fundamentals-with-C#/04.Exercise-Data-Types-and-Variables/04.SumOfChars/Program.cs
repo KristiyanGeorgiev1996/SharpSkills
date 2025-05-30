81b7e42785f49ed1a5457449ce40c380e6493fc7
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-30 17:28:22 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/04.Exercise-Data-Types-and-Variables/04.SumOfChars/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+
+int n = int.Parse(Console.ReadLine());
+
+int sum = 0;
+
+while (n != 0)
+{
+    char letter = char.Parse(Console.ReadLine());
+    int intOfLetter = (int)(letter);
+    sum += intOfLetter;
+    n--;
+}
+Console.WriteLine($"The sum equals: {sum}");
