60b9734a474eb93cf616ec1d0933575db12aae38
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-30 18:32:55 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/07.Methods/10.MultiplyEvensByOdds/Program.cs
@@ -0,0 +1,21 @@
+﻿int input = Math.Abs(int.Parse(Console.ReadLine()));
+int odd = 0;
+int even = 0;
+while (input > 0)
+{
+    int lastDigit = input % 10;
+    if (lastDigit % 2 == 0)
+    {
+        even += lastDigit;
+    }
+
+    else
+    {
+        odd += lastDigit;
+    }
+
+    input /= 10;
+}
+
+int result = even * odd;
+Console.WriteLine(result);
