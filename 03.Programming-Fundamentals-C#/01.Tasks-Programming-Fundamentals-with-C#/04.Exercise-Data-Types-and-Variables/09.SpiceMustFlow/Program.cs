2e5432e5aa4085073b81a2840bca470da605ae33
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-30 17:29:44 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/04.Exercise-Data-Types-and-Variables/09.SpiceMustFlow/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Numerics;
+
+decimal dobivs = decimal.Parse(Console.ReadLine());
+
+decimal days = 0;
+decimal allDobiv = 0;
+
+while (true)
+{
+    if (dobivs >= 100)
+    {
+        days++;
+        decimal n = dobivs - 26;
+        allDobiv += n;
+        dobivs -= 10;
+    }
+    else if (dobivs < 100)
+    {
+        allDobiv -= 26;
+        dobivs -= 10;
+        if (allDobiv <= 0)
+        {
+            allDobiv = 0;
+
+        }
+        dobivs -= 10;
+        break;
+    }
+
+    if (dobivs < 0)
+    {
+        break;
+    }
+}
+
+
+Console.WriteLine(days);
+Console.WriteLine(allDobiv);
