65d2908e86f2b0e7b3a2b7ef5afdf9a691e73f25
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-30 17:02:18 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/02.Exercise-Basic-Syntax-Conditional-Statements-and-Loops/10.RageExpenses/Program.cs
@@ -0,0 +1,39 @@
+﻿int lostGames = int.Parse(Console.ReadLine());
+double priceOfSlushalki = double.Parse(Console.ReadLine());
+double priceOfMouse = double.Parse(Console.ReadLine());
+double priceOfKlaviatura = double.Parse(Console.ReadLine());
+double priceOfDisplay = double.Parse(Console.ReadLine());
+
+double allSumForNew = 0;
+
+if (lostGames >= 2)
+{
+    double breakSlushalki = lostGames / 2;
+    double newSlushalki = Math.Abs(breakSlushalki);
+    double smetka = newSlushalki * priceOfSlushalki;
+    allSumForNew += smetka;
+}
+if (lostGames >= 3)
+{
+    double breakMouses = lostGames / 3;
+    double newMouses = Math.Abs(breakMouses);
+    double smetka = newMouses * priceOfMouse;
+    allSumForNew += smetka;
+}
+if (lostGames >= 6)
+{
+    double breakKlaviatura = lostGames / 6;
+    double newKlaviatura = Math.Abs(breakKlaviatura);
+    double smetka = newKlaviatura * priceOfKlaviatura;
+    allSumForNew += smetka;
+}
+if (lostGames >= 12)
+{
+    double breakDisplay = lostGames / 12;
+    double newDisplay = Math.Abs(breakDisplay);
+    double smetka = newDisplay * priceOfDisplay;
+    allSumForNew += smetka;
+}
+
+Console.WriteLine($"Rage expenses: {allSumForNew:F2} lv.");
+
