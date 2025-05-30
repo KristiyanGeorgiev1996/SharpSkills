4428db15e6995c7146f4a3dcd78a50c2fb08c2ad
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-30 17:30:01 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/04.Exercise-Data-Types-and-Variables/10.PokeMon/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PokeMon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int power = int.Parse(Console.ReadLine());
+            int distance = int.Parse(Console.ReadLine());
+            int exhaustionFactor = int.Parse(Console.ReadLine());
+
+            int restPower = power;
+            int targetsCount = 0;
+
+            while (restPower >= distance)
+            {
+                if (restPower == power * 0.5 && exhaustionFactor != 0)
+                {
+                    restPower /= exhaustionFactor;
+                }
+                if (restPower < distance)
+                {
+                    break;
+                }
+                restPower -= distance;
+                targetsCount++;
+            }
+            Console.WriteLine(restPower);
+            Console.WriteLine(targetsCount);
+        }
+    }
+}
