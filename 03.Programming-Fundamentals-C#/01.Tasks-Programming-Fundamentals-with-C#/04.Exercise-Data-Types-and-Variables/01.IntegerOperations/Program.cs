4d3851769951805544deff1edc84d2c4d5054456
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-30 17:27:32 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/04.Exercise-Data-Types-and-Variables/01.IntegerOperations/Program.cs
@@ -0,0 +1,10 @@
+﻿int first = int.Parse(Console.ReadLine());
+int sec = int.Parse(Console.ReadLine());
+int third = int.Parse(Console.ReadLine());
+int fourth = int.Parse(Console.ReadLine());
+
+double smetka1 = first + sec;
+double smetka2 = smetka1 / third;
+double smetka3 = smetka2 * fourth;
+
+Console.WriteLine(smetka3);
