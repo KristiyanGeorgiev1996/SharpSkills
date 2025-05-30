e124a609ca8508b7f7eeacb3f324b96f5c2613d5
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-30 18:13:00 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/07.Methods/08.MathPower/Program.cs
@@ -0,0 +1,20 @@
+﻿double n = double.Parse(Console.ReadLine());
+double x = double.Parse(Console.ReadLine());
+double sum = 0;
+double totalSum = 0;
+double numbersOfX = x;
+
+MathPower(n, x);
+
+void MathPower(double n, double x)
+{
+    if (n != 0 && x != 0)
+    {
+        double Poq = Math.Pow(n, x);
+        Console.WriteLine(Poq);
+    }
+    else
+    {
+        Console.WriteLine(0);
+    }
+}
