c01a38c6b79470abb6edb8a679a85508c2a26427
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-05-30 17:37:15 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/05.Arrays/02.PrintNumbersOnReverseOrder/Program.cs
@@ -0,0 +1,11 @@
+﻿int numbers = int.Parse(Console.ReadLine());
+int[] array = new int[numbers];
+
+for (int i = 0; i < numbers; i++)
+{
+    array[i] = int.Parse(Console.ReadLine());
+}
+for (int i = 0; i < numbers; i++)
+{
+    Console.Write(array[array.Length - 1 - i] + " ");
+}
