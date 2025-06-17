4936a2ce7df4a8db4589d67e173d57e4f65ed3c4
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-06-17 19:17:41 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/15.Text-Processing/05.DigitsLettersAndOther/Program.cs
@@ -0,0 +1,31 @@
+﻿string someString = Console.ReadLine();
+
+string digits = "";
+string letters = "";
+string cymbols = "";
+
+for (int i = 0; i < someString.Length; i++)
+{
+    if (char.IsLetter(someString[i]))
+    {
+        letters += someString[i];
+    }
+}
+for (int i = 0; i < someString.Length; i++)
+{
+    if (char.IsDigit(someString[i]))
+    {
+        digits += someString[i];
+    }
+}
+for (int i = 0; i < someString.Length; i++)
+{
+    if (!char.IsDigit(someString[i]) && !char.IsLetter(someString[i]))
+    {
+        cymbols += someString[i];
+    }
+}
+
+Console.WriteLine(digits);
+Console.WriteLine(letters);
+Console.WriteLine(cymbols);
