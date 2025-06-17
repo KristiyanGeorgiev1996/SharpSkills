61447bc0c804f836caf303901a010fac7d0b3232
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-06-17 19:08:22 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/14.Exercise-Associative-Arrays/07.CompanyUsers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var companies = new Dictionary<string, HashSet<string>>();
+        string input;
+
+        while ((input = Console.ReadLine()) != "End")
+        {
+            string[] parts = input.Split(" -> ");
+            string companyName = parts[0];
+            string employeeId = parts[1];
+
+            if (!companies.ContainsKey(companyName))
+            {
+                companies[companyName] = new HashSet<string>();
+            }
+
+            companies[companyName].Add(employeeId);
+        }
+
+        foreach (var company in companies)
+        {
+            Console.WriteLine(company.Key);
+            foreach (var employeeId in company.Value)
+            {
+                Console.WriteLine($"-- {employeeId}");
+            }
+        }
+    }
+}
