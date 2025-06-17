2b2dc0aac3dc38b4603a8bd9f8998ec6885f6bc6
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-06-17 19:17:13 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/15.Text-Processing/04.TextFilter/Program.cs
@@ -0,0 +1,10 @@
+﻿string[] bannedStrings = Console.ReadLine().Split(", ");
+string text = Console.ReadLine();
+////string stars = new string('*', 3);
+
+for (int i = 0; i < bannedStrings.Length; i++)
+{
+    text = text.Replace(bannedStrings[i], new string('*', bannedStrings[i].Length));
+}
+
+Console.WriteLine(text);
