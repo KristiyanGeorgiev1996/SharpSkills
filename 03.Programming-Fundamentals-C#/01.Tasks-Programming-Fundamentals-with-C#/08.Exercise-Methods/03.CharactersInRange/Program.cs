f1a9b653bc9b84b692644169d6800cb8b5fea1bb
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-06-17 12:44:23 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/08.Exercise-Methods/03.CharactersInRange/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+char one = char.Parse(Console.ReadLine());
+char two = char.Parse(Console.ReadLine());
+
+if (one < two)
+{
+    for (int i = one + 1; i < two; i++)
+    {
+        Console.Write($"{(char)(i)} ");
+    }
+}
+else if (one > two)
+{
+    for (int i = two + 1; i < one; i++)
+    {
+        Console.Write($"{(char)(i)} ");
+    }
+}
