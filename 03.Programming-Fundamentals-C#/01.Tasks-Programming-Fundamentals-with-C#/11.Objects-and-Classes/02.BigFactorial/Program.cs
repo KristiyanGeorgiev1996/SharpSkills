2c8b28342b0098017d7e9f28b678a7b835ba9d44
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-06-17 13:24:34 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/11.Objects-and-Classes/02.BigFactorial/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Numerics; // Required for handling large numbers
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        Console.WriteLine(Factorial(N));
+    }
+
+    static BigInteger Factorial(int n)
+    {
+        BigInteger result = 1;
+        for (int i = 2; i <= n; i++)
+        {
+            result *= i;
+        }
+        return result;
+    }
+}
