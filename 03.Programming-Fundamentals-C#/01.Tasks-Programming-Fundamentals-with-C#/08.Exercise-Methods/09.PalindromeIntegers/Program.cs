7b6d7382b7e2c23157721c7249405e62b3ec1ad0
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-06-17 12:46:57 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/08.Exercise-Methods/09.PalindromeIntegers/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.ComponentModel.Design;
+
+string text = Console.ReadLine();
+
+while (text != "END")
+{
+    if (text == "END")
+    {
+        break;
+    }
+    if (text.Length == 1)
+    {
+        Console.WriteLine("true");
+        text = Console.ReadLine();
+    }
+    else if (text.Length == 2)
+    {
+        Console.WriteLine("false");
+        text = Console.ReadLine();
+    }
+    else if (text.Length == 3)
+    {
+        if (text[text.Length - 1] == text[text.Length - 3])
+        {
+            Console.WriteLine("true");
+            text = Console.ReadLine();
+        }
+        else
+        {
+            Console.WriteLine("false");
+            text = Console.ReadLine();
+        }
+    }
+    else if (text.Length == 4)
+    {
+        if (text[text.Length - 1] == text[text.Length - 4] && text[text.Length - 2] == text[text.Length - 3])
+        {
+            Console.WriteLine("true");
+            text = Console.ReadLine();
+        }
+        else
+        {
+            Console.WriteLine("false");
+            text = Console.ReadLine();
+        }
+    }
+    else if (text.Length == 5)
+    {
+        if (text[text.Length - 1] == text[text.Length - 5] && text[text.Length - 2] == text[text.Length - 4])
+        {
+            Console.WriteLine("true");
+            text = Console.ReadLine();
+        }
+        else
+        {
+            Console.WriteLine("false");
+            text = Console.ReadLine();
+        }
+    }
+    else if (text.Length == 6)
+    {
+        if (text[text.Length - 1] == text[text.Length - 6] && text[text.Length - 2] == text[text.Length - 5] && text[text.Length - 3] == text[text.Length - 4])
+        {
+            Console.WriteLine("true");
+            text = Console.ReadLine();
+        }
+        else
+        {
+            Console.WriteLine("false");
+            text = Console.ReadLine();
+        }
+    }
+}
