414f31781d8e3bb1474c487bd47fd23851bc9fca
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-06-17 12:45:47 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/08.Exercise-Methods/06.MiddleCharacters/Program.cs
@@ -0,0 +1,20 @@
+﻿string input = Console.ReadLine();
+
+////char middleCharacter = input[input.Length / 2];
+
+MiddleCharacter(input);
+
+void MiddleCharacter(string input)
+{
+    if (input.Length % 2 == 0)
+    {
+        char middleCharacter2 = input[(input.Length / 2) - 1];
+        char middleCharakter = input[input.Length / 2];
+        Console.WriteLine($"{middleCharacter2}{middleCharakter}");
+    }
+    else if (input.Length % 2 == 1)
+    {
+        char middleCharacter = input[input.Length / 2];
+        Console.WriteLine(middleCharacter);
+    }
+}
