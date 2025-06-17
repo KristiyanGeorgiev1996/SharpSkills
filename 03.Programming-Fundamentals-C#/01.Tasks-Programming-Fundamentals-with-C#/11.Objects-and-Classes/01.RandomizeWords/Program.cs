360b99da863e483a7b6b7a0353e874a065402769
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-06-17 13:24:14 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/11.Objects-and-Classes/01.RandomizeWords/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        StringRandomizer randomizer = new StringRandomizer();
+
+        randomizer.Input = Console.ReadLine();
+        randomizer.RandomizeInput();
+
+        foreach (var item in randomizer.RandomizerInput)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
+
+class StringRandomizer
+{
+    public string Input { get; set; }
+    public List<string> RandomizerInput { get; private set; }
+
+    public void RandomizeInput()
+    {
+        RandomizerInput = new List<string>();
+        List<string> splitted = Input.Split(" ").ToList();
+        int count = splitted.Count;
+        Random random = new Random();
+
+        for (int i = 0; i < count; i++)
+        {
+            int index = random.Next(0, splitted.Count);
+            RandomizerInput.Add(splitted[index]);
+            splitted.RemoveAt(index);
+        }
+    }
+}
