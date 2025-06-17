646cc00debadb1cd00c741633f94fc393a585cdf
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-06-17 19:16:00 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/15.Text-Processing/01.ReverseStrings/Program.cs
@@ -0,0 +1,19 @@
+﻿while (true)
+{
+    string text = Console.ReadLine();
+
+    if (text == "end")
+    {
+        break;
+    }
+    else
+    {
+        string reversedText = "";
+
+        for (int i = text.Length - 1; i >= 0; i--)
+        {
+            reversedText += text[i];
+        }
+        Console.WriteLine($"{text} = {reversedText}");
+    }
+}
