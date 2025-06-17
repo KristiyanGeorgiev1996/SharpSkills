4e324e7c457e126972b8ab07259b947ee1322875
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-06-17 19:06:16 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/14.Exercise-Associative-Arrays/02.AMinerTask/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics.Metrics;
+using System.Runtime.InteropServices;
+
+string resource = Console.ReadLine();
+
+Dictionary<string, int> resources = new Dictionary<string, int>();
+
+while (true)
+{
+    if (resource == "stop")
+    {
+        break;
+    }
+    else
+    {
+        int sum = int.Parse(Console.ReadLine());
+        if (!resources.ContainsKey(resource))
+        {
+            resources.Add(resource, sum);
+        }
+        else
+        {
+            resources[resource] += sum;
+        }
+
+    }
+    resource = Console.ReadLine();
+}
+
+foreach (var item in resources)
+{
+    Console.WriteLine($"{item.Key} -> {item.Value}");
+}
+
+//if (!chars.ContainsKey(letter))
+//{
+//    chars.Add(letter, 1);
+//}
+//else
+//{
+//    chars[letter]++;
+//}
