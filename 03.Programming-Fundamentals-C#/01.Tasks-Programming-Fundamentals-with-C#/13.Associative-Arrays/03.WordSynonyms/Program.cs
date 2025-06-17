59da3e809ebeebc733217ee00b47c8609c6f87bf
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-06-17 18:53:55 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/13.Associative-Arrays/03.WordSynonyms/Program.cs
@@ -0,0 +1,20 @@
+﻿Dictionary<string, List<string>> wordSynonims = new Dictionary<string, List<string>>();
+
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    string word = Console.ReadLine();
+    string synonim = Console.ReadLine();
+
+    if (!wordSynonims.ContainsKey(word))
+    {
+        wordSynonims.Add(word, new List<string>());
+    }
+    wordSynonims[word].Add(synonim);
+}
+
+foreach (var wordSynonim in wordSynonims)
+{
+    Console.WriteLine($"{wordSynonim.Key} - {String.Join(", ", wordSynonim.Value)}");
+}
