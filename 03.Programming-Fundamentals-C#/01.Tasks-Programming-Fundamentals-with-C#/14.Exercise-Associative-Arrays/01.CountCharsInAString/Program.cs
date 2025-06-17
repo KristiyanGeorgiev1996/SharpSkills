5ba70269186db432fa40c1f1a15ce9c36c7a2db3
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-06-17 19:05:51 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/14.Exercise-Associative-Arrays/01.CountCharsInAString/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> wordArray = Console.ReadLine().Split(" ").ToList();
+
+        Dictionary<char, int> chars = new();
+
+        for (int i = 0; i < wordArray.Count; i++)
+        {
+            for (int j = 0; j < wordArray[i].Length; j++)
+            {
+                char letter = wordArray[i][j];
+                if (!chars.ContainsKey(letter))
+                {
+                    chars.Add(letter, 1);
+                }
+                else
+                {
+                    chars[letter]++;
+                }
+            }
+        }
+
+        foreach (var letter in chars)
+        {
+            Console.WriteLine($"{letter.Key} -> {letter.Value}");
+        }
+    }
+}
