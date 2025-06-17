86d2c9cddf123cd58ebca9db72378402b5e9e755
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-06-17 19:07:31 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/14.Exercise-Associative-Arrays/05.Courses/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Dictionary<string, List<string>> courses = new Dictionary<string, List<string>>();
+
+        while (true)
+        {
+            string input = Console.ReadLine();
+
+            if (input == "end")
+            {
+                break;
+            }
+
+            string[] parts = input.Split(" : ");
+            string courseName = parts[0];
+            string studentName = parts[1];
+
+            if (!courses.ContainsKey(courseName))
+            {
+                courses[courseName] = new List<string>();
+            }
+
+            courses[courseName].Add(studentName);
+        }
+
+        foreach (var course in courses)
+        {
+            Console.WriteLine($"{course.Key}: {course.Value.Count}");
+            foreach (var student in course.Value)
+            {
+                Console.WriteLine($"-- {student}");
+            }
+        }
+    }
+}
