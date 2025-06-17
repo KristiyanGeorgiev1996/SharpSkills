98ab671ffd9651d769aeadba73c7e9bb939bec28
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-06-17 13:11:48 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/10.Exercise-List/03.HouseParty/Program.cs
@@ -0,0 +1,57 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+List<string> names = new List<string>();
+
+while (n > 0)
+{
+    List<string> nameGoing = Console.ReadLine().Split().ToList();
+    if (nameGoing[nameGoing.Count - 2] == "is")
+    {
+        int count = 0;
+        for (int i = 0; i < names.Count; i++)
+        {
+            if (nameGoing[0] == names[i])
+            {
+                count++;
+                break;
+            }
+        }
+        if (count == 0)
+        {
+            names.Add(nameGoing[0]);
+        }
+        else
+        {
+            Console.WriteLine($"{nameGoing[0]} is already in the list!");
+        }
+        //// names.Add(nameGoing[0]);
+    }
+    else if (nameGoing[nameGoing.Count - 2] == "not")
+    {
+        int count = 0;
+        for (int i = 0; i < names.Count; i++)
+        {
+            if (nameGoing[0] == names[i])
+            {
+                count++;
+                break;
+            }
+
+        }
+        if (count != 0)
+        {
+            names.Remove(nameGoing[0]);
+        }
+        else
+        {
+            Console.WriteLine($"{nameGoing[0]} is not in the list!");
+        }
+    }
+
+    n--;
+}
+
+for (int i = 0; i < names.Count; i++)
+{
+    Console.WriteLine(names[i]);
+}
