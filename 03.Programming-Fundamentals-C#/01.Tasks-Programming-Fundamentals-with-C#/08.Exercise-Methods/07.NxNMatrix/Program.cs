9686dbc064362cb2418b1512c0ecd44e599ae5cd
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-06-17 12:46:12 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/08.Exercise-Methods/07.NxNMatrix/Program.cs
@@ -0,0 +1,11 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+
+for (int i = 0; i < n; i++)
+{
+    for (int k = 1; k < n; k++)
+    {
+        Console.Write(n + " ");
+    }
+    Console.WriteLine(n + " ");
+}
