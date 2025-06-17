5a454d48c9bfc2c50e88b035bbc320fb14a9bf36
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-06-17 13:25:15 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/11.Objects-and-Classes/04.Students/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel;
+
+List<Student> student = new List<Student>();
+
+while (true)
+{
+    string command = Console.ReadLine();
+    if (command == "end")
+    {
+        break;
+    }
+    else
+    {
+        Student studentX = new Student();
+        string[] commandArray = command.Split(" ");
+        string firstName = commandArray[0];
+        string lastName = commandArray[1];
+        string age = commandArray[2];
+        string homeTown = commandArray[3];
+
+        studentX.FirstName = firstName;
+        studentX.LastName = lastName;
+        studentX.Age = age;
+        studentX.HomeTown = homeTown;
+
+        student.Add(studentX);
+    }
+}
+
+string city = Console.ReadLine();
+
+foreach (Student studentX in student)
+{
+    if (studentX.HomeTown == city)
+    {
+        Console.WriteLine($"{studentX.FirstName} {studentX.LastName} is {studentX.Age} years old.");
+    }
+}
+
+class Student
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Age { get; set; }
+    public string HomeTown { get; set; }
+}
