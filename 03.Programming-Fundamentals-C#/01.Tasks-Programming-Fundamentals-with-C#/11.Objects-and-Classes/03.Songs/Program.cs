40c37d621d6ab5b2b1ff3204a9b324e8df1063cd
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-06-17 13:24:55 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/11.Objects-and-Classes/03.Songs/Program.cs
@@ -0,0 +1,49 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+List<Song> songs = new List<Song>();
+
+for (int i = 0; i < n; i++)
+{
+    string[] data = Console.ReadLine().Split("_");
+
+    Song song = new Song();
+
+    string type = data[0];
+    string name = data[1];
+    string date = data[2];
+
+    song.Data = date;
+    song.TypeList = type;
+    song.Name = name;
+
+    songs.Add(song);
+}
+
+string typeList = Console.ReadLine();
+
+if (typeList == "all")
+{
+    foreach (Song song in songs)
+    {
+        Console.WriteLine(song.Name);
+    }
+}
+else
+{
+    foreach (Song song in songs)
+    {
+        if (song.TypeList == typeList)
+        {
+            Console.WriteLine(song.Name);
+        }
+    }
+}
+
+
+class Song
+{
+    public string TypeList { get; set; }
+    public string Name { get; set; }
+    public string Data { get; set; }
+
+}
