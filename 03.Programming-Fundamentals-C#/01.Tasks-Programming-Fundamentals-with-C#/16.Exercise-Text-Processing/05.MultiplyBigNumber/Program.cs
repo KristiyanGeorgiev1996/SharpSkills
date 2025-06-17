9aff869e3333a777d61b8f0684f84a050d3d9f90
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-06-17 19:29:09 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/16.Exercise-Text-Processing/05.MultiplyBigNumber/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Numerics;
+
+BigInteger firstNum = BigInteger.Parse(Console.ReadLine());
+BigInteger secNum = BigInteger.Parse(Console.ReadLine());
+
+BigInteger sum = firstNum * secNum;
+
+Console.WriteLine(sum);
+
