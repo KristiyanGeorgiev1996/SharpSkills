e6d22dee14f222af030623a67953ffa60899ac1c
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-06-17 19:29:37 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/16.Exercise-Text-Processing/06.ReplaceRepeatingChars/Program.cs
@@ -0,0 +1,18 @@
+﻿string text = Console.ReadLine();
+string newText = "";
+
+for (int i = 0; i < text.Length; i++)
+{
+    if (i == 0)
+    {
+        char c = text[i];
+        newText += c;
+    }
+    else if (text[i] != text[i - 1])
+    {
+        char b = text[i];
+        newText += b;
+    }
+}
+
+Console.WriteLine(newText);
