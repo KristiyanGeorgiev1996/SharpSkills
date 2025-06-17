5e01bfbf04e539cf52c158bbc51ad673d1bf8cb5
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-06-17 12:45:20 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/08.Exercise-Methods/05.AddAndSubtract/Program.cs
@@ -0,0 +1,11 @@
+﻿int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+SumOfThreeDigits(a, b, c);
+
+void SumOfThreeDigits(int a, int b, int c)
+{
+    int totalSum = (a + b) - c;
+    Console.WriteLine(totalSum);
+}
