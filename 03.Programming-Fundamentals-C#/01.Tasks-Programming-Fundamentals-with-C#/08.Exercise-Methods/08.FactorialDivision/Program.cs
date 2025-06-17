1cf84012a59932bcefb1e252f35a7c190ccacb93
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-06-17 12:46:36 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/08.Exercise-Methods/08.FactorialDivision/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Numerics;
+
+decimal n = decimal.Parse(Console.ReadLine());
+decimal x = decimal.Parse(Console.ReadLine());
+
+decimal fact = n;
+decimal fact2 = x;
+
+for (decimal i = n; i > 1; i--)
+{
+    fact *= (i - 1);
+}
+
+for (decimal i = x; i > 1; i--)
+{
+    fact2 *= (i - 1);
+}
+
+decimal divide = fact / fact2;
+
+Console.WriteLine($"{divide:F2}");
