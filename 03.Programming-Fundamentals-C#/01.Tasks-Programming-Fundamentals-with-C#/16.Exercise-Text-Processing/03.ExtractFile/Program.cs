060066640f2872b1fa81989a561796ebef3da766
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-06-17 19:28:02 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/16.Exercise-Text-Processing/03.ExtractFile/Program.cs
@@ -0,0 +1,27 @@
+﻿string[] fileName = Console.ReadLine().Split('\\');
+
+int x = fileName.Length - 1;
+int yy = 0;
+
+string name = "";
+string file = "";
+
+for (int i = 0; i < fileName[x].Length; i++)
+{
+    if (fileName[x][i] == '.')
+    {
+        yy = i;
+    }
+}
+
+for (int i = 0; i < yy; i++)
+{
+    name += fileName[x][i];
+}
+for (int i = yy + 1; i < fileName[x].Length; i++)
+{
+    file += fileName[x][i];
+}
+
+Console.WriteLine($"File name: {name}");
+Console.WriteLine($"File extension: {file}");
