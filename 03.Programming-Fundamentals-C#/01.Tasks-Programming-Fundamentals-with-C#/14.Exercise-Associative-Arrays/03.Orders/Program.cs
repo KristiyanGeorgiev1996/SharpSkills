9ddd5aeaa6c5426938191acaafde7fde659476ad
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-06-17 19:06:41 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/14.Exercise-Associative-Arrays/03.Orders/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Data;
+using System.Resources;
+using System.Security.Authentication.ExtendedProtection;
+
+Dictionary<string, (double Price, double Quantity)> price = new();
+
+
+while (true)
+{
+    string command = Console.ReadLine();
+    if (command == "buy")
+    {
+        break;
+    }
+    else
+    {
+        string[] splittedCommand = command.Split();
+        string productName = splittedCommand[0];
+        double cena = double.Parse(splittedCommand[1]);
+        double kolichestvo = double.Parse(splittedCommand[2]);
+        if (!price.ContainsKey(splittedCommand[0]))
+        {
+            price.Add(productName, (cena, kolichestvo));
+        }
+        else
+        {
+            var currentData = price[productName];
+            double newQuantity = currentData.Quantity + kolichestvo;
+            double newPrice = cena;
+            price[productName] = (newPrice, newQuantity);
+
+        }
+    }
+}
+
+foreach (var item in price)
+{
+    double totalValue = item.Value.Price * item.Value.Quantity;
+    Console.WriteLine($"{item.Key} -> {totalValue:F2}");
+}
