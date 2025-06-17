d55cbe3e2c31f2082947b74baff0af4cf9b62ec5
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-06-17 12:43:59 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/08.Exercise-Methods/02.VowelsCount/Program.cs
@@ -0,0 +1,17 @@
+﻿string text = Console.ReadLine();
+////string result = text.Remove(text.Length - 1);
+
+int sum = 0;
+
+////(text[i] == "A" || text[i] == "a" || text[i] == "e" || text[i] == "E" || text[i] == "i" || text[i] == "I" || text[i] == "o" || text[i] == "O" || text[i] == "u" || text[i] == "U")
+
+while (text.Length != 0)
+{
+    char lastCharacter = text[text.Length - 1];
+    if (lastCharacter == 'A' || lastCharacter == 'a' || lastCharacter == 'e' || lastCharacter == 'E' || lastCharacter == 'i' || lastCharacter == 'I' || lastCharacter == 'o' || lastCharacter == 'O' || lastCharacter == 'u' || lastCharacter == 'U')
+    {
+        sum++;
+    }
+    text = text.Remove(text.Length - 1);
+}
+Console.WriteLine(sum);
