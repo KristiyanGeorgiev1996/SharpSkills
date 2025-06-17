be6ebf09d1b4a1db15f0a6ef1927539a8a1e8712
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-06-17 19:40:43 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/17.Regular-Expressions/01.MatchFullName/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class Example
+{
+    public static void Main()
+    {
+        string input = Console.ReadLine();
+        string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+";
+
+        RegexOptions options = RegexOptions.Multiline;
+
+        foreach (Match m in Regex.Matches(input, pattern, options))
+        {
+            Console.Write(m.Value + " ");
+        }
+    }
+}
