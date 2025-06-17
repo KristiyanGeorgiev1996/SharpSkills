0bab886dd42aa28ad91233655748644e9e6cf17a
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-06-17 12:57:34 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/09.List/03.MergingLists/Program.cs
@@ -0,0 +1,30 @@
+﻿List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+List<int> numbers2 = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+List<int> result = new List<int>();
+
+int maxHeigthList = 0;
+
+if (numbers.Count > numbers2.Count)
+{
+    maxHeigthList = numbers.Count;
+}
+else
+{
+    maxHeigthList = numbers2.Count;
+}
+
+for (int i = 0; i < maxHeigthList; i++)
+{
+    //// result.Add(numbers[i]);
+    //// result.Add(numbers2[i]);
+    if (i < numbers.Count)
+    {
+        result.Add(numbers[i]);
+    }
+    if (i < numbers2.Count)
+    {
+        result.Add(numbers2[i]);
+    }
+}
+Console.WriteLine(string.Join(" ", result));
