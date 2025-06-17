fca2fc5932698518e219125a1879f57db98046b1
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-06-17 13:37:29 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/12.Exercise-Objects-and-Classes/03.Articles2.0/Program.cs
@@ -0,0 +1,31 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    string command = Console.ReadLine();
+    string[] commandArray = command.Split(", ");
+
+    List<Article> article = new List<Article>();
+
+    string title = commandArray[0];
+    string content = commandArray[1];
+    string author = commandArray[2];
+
+    Article articleX = new Article();
+
+    articleX.Title = title;
+    articleX.Content = content;
+    articleX.Author = author;
+
+
+    Console.WriteLine($"{articleX.Title} - {articleX.Content}: {articleX.Author}");
+
+}
+
+
+class Article
+{
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public string Author { get; set; }
+}
