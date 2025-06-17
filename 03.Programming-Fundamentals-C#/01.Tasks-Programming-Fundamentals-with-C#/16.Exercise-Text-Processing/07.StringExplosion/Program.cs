890f4e661d4830ed3eb2c0c3e8d1efdbd426f4f8
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-06-17 19:30:01 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/16.Exercise-Text-Processing/07.StringExplosion/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        string text = Console.ReadLine();
+        int explosionStrength = 0;
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (text[i] == '>')
+            {
+                Console.Write('>'); // Print '>'
+                explosionStrength += text[i + 1] - '0'; // Add the strength of the explosion
+            }
+            else if (explosionStrength > 0)
+            {
+                explosionStrength--; // Skip this character
+            }
+            else
+            {
+                Console.Write(text[i]); // Print the character
+            }
+        }
+    }
+}
