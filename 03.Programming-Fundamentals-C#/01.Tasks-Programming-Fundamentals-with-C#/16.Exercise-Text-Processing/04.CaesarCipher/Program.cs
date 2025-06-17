60d5dbe0a6a44cc5bb5e984b8552ebb76dcbd5a0
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-06-17 19:28:26 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/16.Exercise-Text-Processing/04.CaesarCipher/Program.cs
@@ -0,0 +1,9 @@
+﻿string text = Console.ReadLine();
+
+for (int i = 0; i < text.Length; i++)
+{
+    int asciiNumber = (int)text[i];
+    asciiNumber += 3;
+    char character = (char)asciiNumber;
+    Console.Write(character);
+}
