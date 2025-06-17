1dcd267ef4b65376fb04d4da0a37bea2aaf0cc88
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-06-17 13:11:28 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/10.Exercise-List/02.ChangeList/Program.cs
@@ -0,0 +1,28 @@
+﻿List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+while (true)
+{
+    string command = Console.ReadLine();
+    string[] commandArray = command.Split();
+    if (command == "end")
+    {
+        break;
+    }
+
+    if (commandArray[0] == "Delete")
+    {
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            int numberToDelete = int.Parse(commandArray[1]);
+            numbers.Remove(numberToDelete);
+        }
+    }
+    else if (commandArray[0] == "Insert")
+    {
+        int numbertoInsert = int.Parse(commandArray[1]);
+        int indexToInsert = int.Parse(commandArray[2]);
+        numbers.Insert(indexToInsert, numbertoInsert);
+    }
+}
+
+Console.WriteLine(string.Join(" ", numbers));
