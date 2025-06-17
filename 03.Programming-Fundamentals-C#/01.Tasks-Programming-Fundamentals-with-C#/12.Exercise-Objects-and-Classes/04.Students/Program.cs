23ae7475d8392c6c884912d8cf299b8c6c120063
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-06-17 13:37:49 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/01.Tasks-Programming-Fundamentals-with-C#/12.Exercise-Objects-and-Classes/04.Students/Program.cs
@@ -0,0 +1,40 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+List<Student> students = new List<Student>();
+
+
+for (int i = 0; i < n; i++)
+{
+    Student student = new Student();
+    string command = Console.ReadLine();
+    string[] splittedCommand = command.Split();
+
+
+    string firstName = splittedCommand[0];
+    string lastName = splittedCommand[1];
+    string grade = splittedCommand[2];
+    double myStr = double.Parse(grade);
+
+    student.FirstName = firstName;
+    student.LastName = lastName;
+    student.Grade = myStr;
+
+    students.Add(student);
+
+}
+students = students.OrderByDescending(a => a.Grade).ToList();
+foreach (Student student in students)
+{
+    Console.WriteLine($"{student.FirstName} {student.LastName}: {student.Grade:F2}");
+}
+
+
+
+
+
+class Student
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public double Grade { get; set; }
+}
