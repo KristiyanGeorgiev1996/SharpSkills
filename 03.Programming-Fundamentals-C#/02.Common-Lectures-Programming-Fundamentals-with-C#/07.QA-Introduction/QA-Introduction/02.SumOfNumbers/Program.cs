0dbd99facb360a91e7118446ec1f9181b7ae2751
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-08-15 11:44:43 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/02.Common-Lectures-Programming-Fundamentals-with-C#/07.QA-Introduction/QA-Introduction/02.SumOfNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter first number: ");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter third number: ");
+        int num3 = int.Parse(Console.ReadLine());
+
+        int sum = num1 + num2 + num3;
+        Console.WriteLine($"The sum is: {sum}");
+    }
+}
