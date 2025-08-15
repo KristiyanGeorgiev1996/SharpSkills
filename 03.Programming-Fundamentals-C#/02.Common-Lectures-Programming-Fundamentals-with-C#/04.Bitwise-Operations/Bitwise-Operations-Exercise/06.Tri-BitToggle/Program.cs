c1c0999c4625385996f7d75d20b8b076804cc50f
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-08-15 11:05:03 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/02.Common-Lectures-Programming-Fundamentals-with-C#/04.Bitwise-Operations/Bitwise-Operations-Exercise/06.Tri-BitToggle/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class TriBitSwitch
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int p = int.Parse(Console.ReadLine());
+
+        int mask = 7 << p; // 111 shifted to position p
+        int result = n ^ mask;
+
+        Console.WriteLine(result);
+    }
+}
