057f23acbf293785fcf74169464ce4ab8aa841a5
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-08-15 11:03:22 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/02.Common-Lectures-Programming-Fundamentals-with-C#/04.Bitwise-Operations/Bitwise-Operations-Exercise/02.InspectBitAtPosition1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class BitAtPosition1
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int bitAtPosition1 = (n >> 1) & 1;
+
+        Console.WriteLine(bitAtPosition1);
+    }
+}
