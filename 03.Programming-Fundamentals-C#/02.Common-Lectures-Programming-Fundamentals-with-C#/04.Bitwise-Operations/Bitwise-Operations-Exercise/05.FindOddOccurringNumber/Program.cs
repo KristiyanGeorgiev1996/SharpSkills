92a53f38dc607755f5a49984f9d2e388a71fb1e7
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-08-15 11:04:42 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/02.Common-Lectures-Programming-Fundamentals-with-C#/04.Bitwise-Operations/Bitwise-Operations-Exercise/05.FindOddOccurringNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+class OddTimes
+{
+    static void Main()
+    {
+        int[] numbers = Console.ReadLine()
+                               .Split(' ')
+                               .Select(int.Parse)
+                               .ToArray();
+
+        int result = 0;
+        foreach (int num in numbers)
+        {
+            result ^= num;
+        }
+
+        Console.WriteLine(result);
+    }
+}
