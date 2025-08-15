4d9140047dccba88f64d07f72d7d22cd25c7a8bf
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-08-15 11:04:17 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/02.Common-Lectures-Programming-Fundamentals-with-C#/04.Bitwise-Operations/Bitwise-Operations-Exercise/04.ClearBitAtPosition/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class ClearBit
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int p = int.Parse(Console.ReadLine());
+
+        int mask = ~(1 << p);
+        int newNumber = n & mask;
+
+        Console.WriteLine(newNumber);
+    }
+}
