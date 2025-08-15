1348e7a1c4d0ee624e64e0d32ef9f81675d2a3d3
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-08-15 11:12:10 +0300

    Add files via upload

---
+++ 03.Programming-Fundamentals-C#/02.Common-Lectures-Programming-Fundamentals-with-C#/05.Problem-Solving/Problem-Solving-Exercise/SumOfEvenNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+class SumEvenNumbers
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter numbers separated by space:");
+        int[] numbers = Console.ReadLine()
+                               .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                               .Select(int.Parse)
+                               .ToArray();
+
+        int sumEven = 0;
+        foreach (int number in numbers)
+        {
+            if (number % 2 == 0)
+            {
+                sumEven += number;
+            }
+        }
+
+        Console.WriteLine($"Sum of even numbers: {sumEven}");
+    }
+}
